5aded5f86e82839a0d321b20b6a66367c4d34d86
Andreas Vogel <andreas@wellenvogel.net>
2013-04-05 15:44:03 +0000

    info updates
    git-svn-id: svn://10.222.10.12/src1/avnav@367 981887c0-d8ae-4743-aaa6-7abdade54d40

--- avnav_chartconvert/mpp.py
+++ avnav_chartconvert/mpp.py
@@ -2,12 +2,24 @@
 #
 # vim: ts=2 sw=2 et
 #
+import sys
+from wx.py.crust import Display
 
-start= 20037508.342789244 * 2 / 256 
-print "start meters/pixel(level 0):",start
-for i in range(1,32):
-  start=start/2
-  print "level(",i,"):",start
+inchpm=39.3700
+dpi=100
+if len(sys.argv) >1:
+  dpi=int(sys.argv[1])
+
+displaympp=1/(float(dpi)*inchpm)
+
+print "display mpp=%f"%(displaympp)  
+
+mpp= 20037508.342789244 * 2 / 256 
+print "Level    :   mpp     \t\t: scale"
+for i in range(0,31):
+  scale=mpp/displaympp
+  print "level(%02d):%07.4f:\t\t1:%5.2f"%(i,mpp,scale)
+  mpp=mpp/2
 
 
   
