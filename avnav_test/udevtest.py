9c988701553d7aaaffa7a52b3bbe26c42849282e
Andreas Vogel <andreas@wellenvogel.net>
2013-04-05 15:54:40 +0000

    move test parts to avnav_test
    git-svn-id: svn://10.222.10.12/src1/avnav@373 981887c0-d8ae-4743-aaa6-7abdade54d40

---
+++ avnav_test/udevtest.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import pyudev
+import sys
+import pprint
+import re
+import time
+context=pyudev.Context()
+allDev=context.list_devices(subsystem='tty')
+
+
+for dev in allDev:
+  if dev.parent is None or not dev.parent.subsystem == "usb-serial":
+    continue
+  pprint.pprint(dev)
+  print "subsystem:",dev.subsystem,"driver:",dev.driver,"device_type:",dev.device_type,"device_node:",dev.device_node,"parent:",dev.parent
+  monitor = pyudev.Monitor.from_netlink(context)
+  monitor.filter_by(subsystem='tty')
+  #observer = pyudev.MonitorObserver(monitor, callback=print_device_event, name='monitor-observer')
+  print "Enter Wait Loop"
+  for deviceaction in monitor:
+    action,device=deviceaction
+    print 'background event:',action,'device:',device
