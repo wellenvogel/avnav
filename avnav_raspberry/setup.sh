5c6682acb6ec22ee1f718b590fbd5de2c6934a3b
Andreas Vogel <andreas@wellenvogel.net>
2013-04-06 21:19:12 +0000

    1st working version
    git-svn-id: svn://10.222.10.12/src1/avnav@383 981887c0-d8ae-4743-aaa6-7abdade54d40

--- avnav_raspberry/setup.sh
+++ avnav_raspberry/setup.sh
@@ -1,1 +1,37 @@
 #! /bin/sh
+
+err(){
+  echo "ERROR: $*"
+  exit 1
+}
+i=`id -u`
+[ "$i" != "0" ] && err "this must be run as root"
+pdir=`dirname $0`
+pdir=`readlink -f $pdir`
+for serv in check_parts avnav
+do
+  if [ ! -h /etc/init.d/$serv ] ; then
+    rm -f /etc/init.d/$serv
+    echo "creating /etc/init.d/$serv"
+    ln -s $pdir/$serv /etc/init.d/$serv
+    chmod 755 $pdir/$serv
+  else
+    echo "/etc/init.d/$serv is already linked"
+  fi
+	
+done
+
+for d in ../tracks ../logs ../charts
+do
+  d=$pdir/$d
+  if [ -d $d -o -f $d ] ; then
+    echo "moving old $d to $d.old"
+    mv $d $d.old || err "unable to remove $d"
+  fi
+done
+
+echo "starting partitioning check"
+/etc/init.d/check_parts worker
+
+
+
