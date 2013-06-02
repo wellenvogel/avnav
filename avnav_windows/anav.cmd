019d08e5801319d404c0ba2e6a4dc533e4fbd115
Andreas Vogel <andreas@wellenvogel.net>
2013-06-02 09:16:35 +0000

    initial windows startup batch file
    git-svn-id: svn://10.222.10.12/src1/avnav@442 981887c0-d8ae-4743-aaa6-7abdade54d40

---
+++ avnav_windows/anav.cmd
@@ -0,0 +1,9 @@
+rem start anav server on windows
+rem please adapt the avnav_server.xml to your needs
+rem python must be in the PATH (or set this here)
+PATH=%PATH%;"c:\Program Files\python273"
+rem uncomment the line below to enable debug output
+rem or start the batch file with -d
+rem set debug=-d
+echo starting anav server
+python.exe ..\avnav_server\avnav_server.py %debug% %1 avnav_server.xml
