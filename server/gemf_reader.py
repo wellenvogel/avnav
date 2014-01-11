5dc9b9746cd12c880d57edeb4d31c05a85776a0b
Andreas Vogel <andreas@wellenvogel.net>
2014-01-11 21:00:13 +0000

    first tests for gemf
    git-svn-id: svn://10.222.10.12/src1/avnav@577 981887c0-d8ae-4743-aaa6-7abdade54d40

---
+++ server/gemf_reader.py
@@ -0,0 +1,202 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=2 sw=2 et ai
+###############################################################################
+# Copyright (c) 2012,2013 Andreas Vogel andreas@wellenvogel.net
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+#
+###############################################################################
+# read gemf files and provide them for access via http
+# see http://www.cgtk.co.uk/gemf
+import sys
+import os
+import struct
+import threading
+
+class GemfFile():
+  def __init__(self,filename):
+    self.filename=filename
+    self.handles=[]
+    self.sources=[]
+    self.ranges=[]
+    self.isOpen=False
+    self.numsources=0
+    self.rangenum=0
+    self.lock=threading.Lock()
+  #open the file and read the header
+  def open(self):
+    if self.isOpen:
+      raise Exception("gemf file %s already open" % (self.filename))
+    if not os.path.isfile(self.filename):
+      raise Exception("gemf file %s not found" %(self.filename))
+    handle=open(self.filename,"rb")
+    if handle is None:
+      raise Exception("unable to open gemf file %s" %(self.filename))
+    #version,tilesize
+    buf=handle.read(12) 
+    version,tilesize,self.numsources=struct.unpack_from("!3l",buf,0)
+    if version != 4:
+      raise Exception("file %s: invalid gemf version detected %d (expected 4)" %(self.filename,version))
+    if tilesize != 256:
+      raise Exception("file %s: invalid tilesize detected %d (expected 256)" %(self.filename,tilesize))
+    if self.numsources < 0 or self.numsources > 100:
+      raise Exception("file %s: invalid number of sources %d (expected 1...100)" %(self.filename,self.numsources))
+    for i in range(self.numsources):
+      buf=handle.read(8)
+      idx,namelen=struct.unpack_from("!2l",buf,0)
+      if namelen < 0 or namelen > 10000:
+        raise Exception("file %s source #%d (idx=%d) invalid namelen %d (expected 0...10000)" %(self.filename,i,idx,namelen))
+      name=""
+      if (namelen > 0):
+        buf=handle.read(namelen)
+        (name,)=struct.unpack_from("!%ds"%namelen,buf,0)
+      source={'num':i,'idx':idx,'name':name}
+      self.sources.append(source)
+    #rangenum
+    buf=handle.read(4)
+    (self.rangenum,)=struct.unpack_from("!l",buf,0)
+    if self.rangenum < 0:
+      raise Exception("file %s invalid number of ranges %d - expected > 0" %(self.filename,self.rangenum))
+    for ra in range(self.rangenum):
+      buf=handle.read(32)
+      zoom,xmin,xmax,ymin,ymax,srcidx,offset=struct.unpack_from("!6lq",buf,0)
+      #checks???
+      idx=-1
+      for i in range(len(self.sources)):
+        s=self.sources[i]
+        if s['idx'] == srcidx:
+          idx=i
+          break
+      if idx < 0:
+        raise Exception("file %s, range %d - source idx %d not found"%(self.filename,ra,srcidx))
+      rdata={'zoom':zoom,'xmin':xmin,'xmax':xmax,'ymin':ymin,'ymax':ymax,'idx':idx,'offset':offset}
+      self.ranges.append(rdata)
+    self.handles.append(handle)
+    self.isOpen=True
+    #todo: open other files...
+
+  #find a range for a tile
+  #a tile is a tuple zxy
+  #source - the name of the source
+  #return the range data
+  def findRangeForTile(self,tile,source):
+    idx=-1
+    for s in self.sources:
+      if s['name'] == source:
+        idx=s['num']
+        break
+    if idx < 0:
+      return None
+    return self.findRangeForTileByIdx(tile,idx)
+
+  #find a range for a tile given the source idx
+  def findRangeForTileByIdx(self,tile,idx):
+    z,x,y=tile
+    for rdata in self.ranges:
+      if idx != rdata['idx']:
+        continue
+      if z != rdata['zoom']:
+        continue
+      if x > rdata['xmax'] or x < rdata['xmin']:
+        continue
+      if y > rdata['ymax'] or y < rdata['ymin']:
+        continue
+      return rdata
+    return None
+
+  def getTileOffsetLen(self,tile,source):
+    rdata=self.findRangeForTile(tile,source)
+    if rdata is None:
+      return (None,None)
+    z,x,y=tile
+    ynum=rdata['ymax']-rdata['ymin']+1;
+    idxy=y-rdata['ymin']
+    idxx=x-rdata['xmin']
+    idxr=idxx*ynum+idxx;
+    #each range entry has 12 bytes (offset 8 , len 4)
+    offset=12*idxr+rdata['offset']
+    try:
+      self.lock.acquire()
+      self.handles[0].fseek(offset)
+      buf=self.handles[0].read(12)
+      self.lock.release()
+      offset,flen=struct.unpack_from("!ql",buf,0)
+      return (offset,flen)
+    except:
+      self.lock.release()
+      raise
+      
+
+  def getTileData(self,tile,source):
+    offset,flen=self.getTileOffsetLen(tile,source)
+    if offset is None or flen is None:
+      return None
+    try:
+      #todo: handle multiple files
+      self.lock.acquire()
+      self.handles[0].fseek(offset)
+      buf=self.handles[0].read(flen)
+      self.lock.release()
+      return buf
+    except:
+      self.lock.release()
+      raise
+
+  #get a list of sources and their assigned ranges
+  def getSources(self):
+    if not self.isOpen:
+      raise Exception("GEMF file %s not open" %(self.filename))
+    rt=[]
+    for s in self.sources:
+      src=s.copy()
+      ranges=[]
+      for rdata in self.ranges:
+        if rdata['idx'] == src['num']:
+          r={'xmin':rdata['xmin'],
+            'ymin':rdata['ymin'],
+            'xmax':rdata['xmax'],
+            'ymax':rdata['ymax'],
+            'zoom':rdata['zoom']}
+          ranges.append(r)
+      src['ranges']=ranges
+      rt.append(src)
+    return rt
+
+  def close(self):
+    if not self.isOpen:
+      return
+    self.handles[0].close()
+    self.handles=[]
+    self.ranges=[]
+    self.sources=[]
+    self.rangenum=0
+    self.isOpen=False
+
+
+  def __str__(self):
+    rt="GEMF %s (srcnum=%d:" %(self.filename,self.numsources)
+    for s in self.sources:
+      rt+="%(num)d,%(idx)d,%(name)s;" % s
+    rt+=")"
+    rt+=" %d ranges" % self.rangenum
+    return rt
+
+
+
