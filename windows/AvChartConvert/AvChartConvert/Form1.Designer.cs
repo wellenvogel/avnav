959fa5558ffe5ee0dbc1d16e76fe2df9aa7a5dcc
wellenvogel <andreas@wellenvogel.net>
2014-01-12 18:36:58 +0100

    windows app

---
+++ windows/AvChartConvert/AvChartConvert/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace AvChartConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textIn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openInputDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonAddFile = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.checkBoxUpdate = new System.Windows.Forms.CheckBox();
+            this.textOutdir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOutDir = new System.Windows.Forms.Button();
+            this.folderBrowserInput = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonAddDirectories = new System.Windows.Forms.Button();
+            this.folderBrowserOutput = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonEmpty = new System.Windows.Forms.Button();
+            this.buttonDefaultOut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textIn
+            // 
+            this.textIn.Location = new System.Drawing.Point(12, 36);
+            this.textIn.Multiline = true;
+            this.textIn.Name = "textIn";
+            this.textIn.Size = new System.Drawing.Size(493, 321);
+            this.textIn.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "InputFiles";
+            // 
+            // openInputDialog
+            // 
+            this.openInputDialog.FileName = "openFileDialog1";
+            // 
+            // buttonAddFile
+            // 
+            this.buttonAddFile.Location = new System.Drawing.Point(529, 63);
+            this.buttonAddFile.Name = "buttonAddFile";
+            this.buttonAddFile.Size = new System.Drawing.Size(94, 23);
+            this.buttonAddFile.TabIndex = 2;
+            this.buttonAddFile.Text = "AddFile(s)";
+            this.buttonAddFile.UseVisualStyleBackColor = true;
+            this.buttonAddFile.Click += new System.EventHandler(this.buttonAddFile_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(364, 550);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 3;
+            this.buttonOK.Text = "Start";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(171, 550);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Exit";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // checkBoxUpdate
+            // 
+            this.checkBoxUpdate.AutoSize = true;
+            this.checkBoxUpdate.Checked = true;
+            this.checkBoxUpdate.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxUpdate.Location = new System.Drawing.Point(12, 431);
+            this.checkBoxUpdate.Name = "checkBoxUpdate";
+            this.checkBoxUpdate.Size = new System.Drawing.Size(86, 17);
+            this.checkBoxUpdate.TabIndex = 5;
+            this.checkBoxUpdate.Text = "updateMode";
+            this.checkBoxUpdate.UseVisualStyleBackColor = true;
+            // 
+            // textOutdir
+            // 
+            this.textOutdir.Location = new System.Drawing.Point(12, 389);
+            this.textOutdir.Name = "textOutdir";
+            this.textOutdir.Size = new System.Drawing.Size(493, 20);
+            this.textOutdir.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "OutputDir";
+            // 
+            // buttonOutDir
+            // 
+            this.buttonOutDir.Location = new System.Drawing.Point(511, 389);
+            this.buttonOutDir.Name = "buttonOutDir";
+            this.buttonOutDir.Size = new System.Drawing.Size(54, 23);
+            this.buttonOutDir.TabIndex = 8;
+            this.buttonOutDir.Text = "Change";
+            this.buttonOutDir.UseVisualStyleBackColor = true;
+            this.buttonOutDir.Click += new System.EventHandler(this.buttonOutDir_Click);
+            // 
+            // buttonAddDirectories
+            // 
+            this.buttonAddDirectories.Location = new System.Drawing.Point(529, 34);
+            this.buttonAddDirectories.Name = "buttonAddDirectories";
+            this.buttonAddDirectories.Size = new System.Drawing.Size(94, 23);
+            this.buttonAddDirectories.TabIndex = 9;
+            this.buttonAddDirectories.Text = "AddDirectories";
+            this.buttonAddDirectories.UseVisualStyleBackColor = true;
+            this.buttonAddDirectories.Click += new System.EventHandler(this.buttonAddDirectories_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(270, 550);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 10;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonEmpty
+            // 
+            this.buttonEmpty.Location = new System.Drawing.Point(529, 130);
+            this.buttonEmpty.Name = "buttonEmpty";
+            this.buttonEmpty.Size = new System.Drawing.Size(94, 24);
+            this.buttonEmpty.TabIndex = 11;
+            this.buttonEmpty.Text = "Empty";
+            this.buttonEmpty.UseVisualStyleBackColor = true;
+            this.buttonEmpty.Click += new System.EventHandler(this.buttonEmpty_Click);
+            // 
+            // buttonDefaultOut
+            // 
+            this.buttonDefaultOut.Location = new System.Drawing.Point(571, 389);
+            this.buttonDefaultOut.Name = "buttonDefaultOut";
+            this.buttonDefaultOut.Size = new System.Drawing.Size(52, 23);
+            this.buttonDefaultOut.TabIndex = 12;
+            this.buttonDefaultOut.Text = "Default";
+            this.buttonDefaultOut.UseVisualStyleBackColor = true;
+            this.buttonDefaultOut.Click += new System.EventHandler(this.buttonDefaultOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 607);
+            this.Controls.Add(this.buttonDefaultOut);
+            this.Controls.Add(this.buttonEmpty);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonAddDirectories);
+            this.Controls.Add(this.buttonOutDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textOutdir);
+            this.Controls.Add(this.checkBoxUpdate);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonAddFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textIn);
+            this.Name = "Form1";
+            this.Text = "AvChartConvert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openInputDialog;
+        private System.Windows.Forms.Button buttonAddFile;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.CheckBox checkBoxUpdate;
+        private System.Windows.Forms.TextBox textOutdir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOutDir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserInput;
+        private System.Windows.Forms.Button buttonAddDirectories;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserOutput;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonEmpty;
+        private System.Windows.Forms.Button buttonDefaultOut;
+    }
+}
+
---
+++ windows/AvChartConvert/AvChartConvert/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+
+
+namespace AvChartConvert
+{
+    //taken from http://stackoverflow.com/questions/5901679/kill-process-tree-programatically-in-c-sharp
+    class ProcessUtilities
+    {
+        public static void KillProcessTree(Process root)
+        {
+            if (root != null)
+            {
+                var list = new List<Process>();
+                GetProcessAndChildren(Process.GetProcesses(), root, list, 1);
+
+                foreach (Process p in list)
+                {
+                    try
+                    {
+                        p.Kill();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log error?
+                    }
+                }
+            }
+        }
+
+        private static int GetParentProcessId(Process p)
+        {
+            int parentId = 0;
+            try
+            {
+                ManagementObject mo = new ManagementObject("win32_process.handle='" + p.Id + "'");
+                mo.Get();
+                parentId = Convert.ToInt32(mo["ParentProcessId"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                parentId = 0;
+            }
+            return parentId;
+        }
+
+        private static void GetProcessAndChildren(Process[] plist, Process parent, List<Process> output, int indent)
+        {
+            foreach (Process p in plist)
+            {
+                if (GetParentProcessId(p) == parent.Id)
+                {
+                    GetProcessAndChildren(plist, p, output, indent + 1);
+                }
+            }
+            output.Add(parent);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        const String BASE = "AvNavCharts";
+        String defaultOut = null;
+        Process converter = null;
+        public Form1()
+        {
+            InitializeComponent();
+            defaultOut= Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\"+BASE;
+            this.textOutdir.Text = defaultOut;
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1){
+                for (int i = 1; i < args.Length;i++ )
+                {
+                    this.textIn.AppendText(args[i] + "\n");
+                }
+                this.buttonOK_Click(null, null);
+            }
+        }
+
+        private void buttonAddFile_Click(object sender, EventArgs e)
+        {
+            this.openInputDialog.Title = "Select Files or Directories";
+            this.openInputDialog.Multiselect = true;
+            this.openInputDialog.FileName = "Folder Selection";
+            this.openInputDialog.ValidateNames = false;
+            this.openInputDialog.CheckFileExists = false;
+            this.openInputDialog.CheckPathExists = false;
+            this.openInputDialog.Filter = string.Empty;
+            if (this.openInputDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String fn in this.openInputDialog.FileNames){
+                    this.textIn.AppendText(fn + "\n");
+                }
+            }
+            
+
+        }
+
+        private void buttonAddDirectories_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserInput.ShowDialog() == DialogResult.OK)
+            {
+                this.textIn.AppendText(this.folderBrowserInput.SelectedPath + "\n");
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.buttonStop_Click(null, null);
+            this.Close();
+        }
+
+        private void buttonOutDir_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserOutput.SelectedPath = this.textOutdir.Text;
+            if (this.folderBrowserInput.ShowDialog() == DialogResult.OK)
+            {
+                this.textOutdir.Text = this.folderBrowserInput.SelectedPath;
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if (converter != null)
+            {
+                if (converter.HasExited)
+                {
+                    converter = null;
+                }
+                else
+                {
+                    MessageBox.Show("Converter already running");
+                }
+            }
+            Process p = new Process();
+            try
+                {
+                    String myPath = System.IO.Path.GetDirectoryName(
+          System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", "");
+                    String cmd = myPath + "\\..\\..\\..\\..\\AvChartConvert.cmd";
+                    //MessageBox.Show("CMD:" + cmd);
+                    ProcessStartInfo info = new ProcessStartInfo("cmd.exe");
+                    String args = "/K " + cmd;
+                    if (!this.checkBoxUpdate.Checked) args += " -f";
+                    args += " -b " + "\"" + this.textOutdir.Text + "\"";
+                    foreach (String inf in this.textIn.Text.Split('\n'))
+                    {
+                        args += " \"" + inf + "\"";
+                    }
+                    info.Arguments = args;
+                    info.RedirectStandardInput = false;
+                    info.RedirectStandardOutput = false;
+                    info.UseShellExecute = true;
+                    p.StartInfo = info;
+                    p.Start();
+                    converter = p;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Exception when starting:" + exc.Message);
+                }
+                
+                // process output
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (converter != null)
+            {
+                try
+                {
+                    converter.Refresh();
+                    if (converter.HasExited)
+                    {
+                        converter.Refresh();
+                        converter = null;
+                    }
+                    
+                }
+                catch (Exception ex1) {
+                    String txt = ex1.Message;
+                }
+                
+            }
+            this.buttonOK.Enabled = (converter == null);
+            this.buttonStop.Enabled = (converter != null);
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (converter != null)
+            {
+                try
+                {
+                    ProcessUtilities.KillProcessTree(converter);
+                }
+                catch (Exception exc) {
+                    String txt = exc.Message;
+                }
+            }
+        }
+
+        private void buttonDefaultOut_Click(object sender, EventArgs e)
+        {
+            this.textOutdir.Text = defaultOut;
+        }
+
+        private void buttonEmpty_Click(object sender, EventArgs e)
+        {
+            this.textIn.Clear();
+        }
+    }
+}
