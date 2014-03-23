c9f3fd56d5910741f600727927d4841ec5670b24
wellenvogel <andreas@wellenvogel.net>
2014-03-23 13:15:36 +0100

    first version of mappack that works with BSH/OpenSeaMap

---
+++ mobac/testsrc/src/mobac/mapsources/mappacks/avnavbase/ExCustomMapSource.java
@@ -0,0 +1,204 @@
+package mobac.mapsources.mappacks.avnavbase;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlList;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import mobac.exceptions.UnrecoverableDownloadException;
+import mobac.mapsources.MapSourceTools;
+import mobac.mapsources.mapspace.MercatorPower2MapSpace;
+import mobac.program.download.TileDownLoader;
+import mobac.program.interfaces.HttpMapSource;
+import mobac.program.interfaces.MapSourceListener;
+import mobac.program.interfaces.MapSpace;
+import mobac.program.jaxb.ColorAdapter;
+import mobac.program.model.MapSourceLoaderInfo;
+import mobac.program.model.TileImageType;
+import mobac.program.tilestore.TileStore;
+import mobac.program.tilestore.TileStoreEntry;
+
+@XmlRootElement
+public class ExCustomMapSource implements HttpMapSource {
+	
+	@XmlElement(required = false, defaultValue="1")
+	protected int retries = 1;
+	
+	@XmlElement(nillable = false, defaultValue = "Custom")
+	private String name = "Custom";
+
+	@XmlElement(defaultValue = "0")
+	protected int minZoom = 0;
+
+	@XmlElement(required = true)
+	protected int maxZoom = 0;
+
+	@XmlElement(defaultValue = "PNG")
+	protected TileImageType tileType = TileImageType.PNG;
+
+	@XmlElement(defaultValue = "NONE")
+	protected HttpMapSource.TileUpdate tileUpdate;
+
+	@XmlElement(required = true, nillable = false)
+	protected String url = "http://127.0.0.1/{$x}_{$y}_{$z}";
+
+	@XmlElement(defaultValue = "false")
+	private boolean invertYCoordinate = false;
+
+	@XmlElement(defaultValue = "#000000")
+	@XmlJavaTypeAdapter(ColorAdapter.class)
+	protected Color backgroundColor = Color.BLACK;
+
+	@XmlElement(required = false, defaultValue = "false")
+	protected boolean ignoreErrors = false;
+
+	@XmlElement(required = false, defaultValue = "")
+	@XmlList
+	private String[] serverParts = null;
+	private int currentServerPart = 0;
+
+	private MapSourceLoaderInfo loaderInfo = null;
+
+	/**
+	 * Constructor without parameters - required by JAXB
+	 */
+	protected ExCustomMapSource() {
+	}
+
+	public ExCustomMapSource(String name, String url) {
+		this.name = name;
+		this.url = url;
+	}
+
+	public TileUpdate getTileUpdate() {
+		return tileUpdate;
+	}
+
+	public int getMaxZoom() {
+		return maxZoom;
+	}
+
+	public int getMinZoom() {
+		return minZoom;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getStoreName() {
+		return name;
+	}
+
+	public TileImageType getTileImageType() {
+		return tileType;
+	}
+
+	public HttpURLConnection getTileUrlConnection(int zoom, int tilex, int tiley) throws IOException {
+		String url = getTileUrl(zoom, tilex, tiley);
+		if (url == null)
+			return null;
+		return (HttpURLConnection) new URL(url).openConnection();
+	}
+
+	public String getTileUrl(int zoom, int tilex, int tiley) {
+		if (serverParts == null || serverParts.length == 0) {
+			return MapSourceTools.formatMapUrl(url, zoom, tilex, tiley);
+		} else {
+			currentServerPart = (currentServerPart + 1) % serverParts.length;
+			String serverPart = serverParts[currentServerPart];
+			return MapSourceTools.formatMapUrl(url, serverPart, zoom, tilex, tiley);
+		}
+	}
+
+	public byte[] getTileData(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
+			UnrecoverableDownloadException, InterruptedException {
+		if (invertYCoordinate)
+			y = ((1 << zoom) - y - 1);
+
+		if (loadMethod == LoadMethod.CACHE) {
+			TileStoreEntry entry = TileStore.getInstance().getTile(x, y, zoom, this);
+			if (entry == null)
+				return null;
+			byte[] data = entry.getData();
+			if (Thread.currentThread() instanceof MapSourceListener) {
+				((MapSourceListener) Thread.currentThread()).tileDownloaded(data.length);
+			}
+			return data;
+		}
+		if (!ignoreErrors && ! (retries > 1))
+			return TileDownLoader.getImage(x, y, zoom, this);
+		else
+			try {
+				return TileDownLoader.getImage(x, y, zoom, this);
+			} catch (Exception e) {
+				return null;
+			}
+	}
+
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	public MapSpace getMapSpace() {
+		return MercatorPower2MapSpace.INSTANCE_256;
+	}
+
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	@XmlTransient
+	public MapSourceLoaderInfo getLoaderInfo() {
+		return loaderInfo;
+	}
+
+	public void setLoaderInfo(MapSourceLoaderInfo loaderInfo) {
+		if (this.loaderInfo != null)
+			throw new RuntimeException("LoaderInfo already set");
+		this.loaderInfo = loaderInfo;
+	}
+
+	/**
+	 * get image with extended error handling and retries
+	 */
+	
+	public BufferedImage getTileImage(int zoom, int x, int y, LoadMethod loadMethod)
+			throws IOException, UnrecoverableDownloadException, InterruptedException {
+		for (int ntry = 0; ntry < retries; ntry++) {
+			byte[] data = getTileData(zoom, x, y, loadMethod);
+			if (data == null || data.length == 0) {
+				if (loadMethod == LoadMethod.CACHE)
+					return null;
+				continue;
+			}
+			return ImageIO.read(new ByteArrayInputStream(data));
+		}
+		if (!ignoreErrors)
+			return null;
+		BufferedImage image = new BufferedImage(256, 256, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics g = (Graphics) image.getGraphics();
+		try {
+			// transparent background
+			g.setColor(new Color(0, 0, 0, 0));
+			g.fillRect(0, 0, 256, 256);
+		} finally {
+			g.dispose();
+		}
+		return image;
+	}	
+	
+
+}
---
+++ mobac/testsrc/src/mobac/mapsources/mappacks/avnavbase/ExCustomWmsMapSource.java
@@ -0,0 +1,109 @@
+package mobac.mapsources.mappacks.avnavbase;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlElements;
+import javax.xml.bind.annotation.XmlList;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import mobac.mapsources.MapSourceTools;
+import mobac.mapsources.custom.CustomMapSource;
+import mobac.program.jaxb.ColorAdapter;
+
+/**
+ * extend the CustomWmsMapSource by some features...
+ */
+@XmlRootElement
+public class ExCustomWmsMapSource extends ExCustomMapSource {
+	
+	public static class LayerMapping{
+		public String zoom;
+		public String layers;
+	}
+	
+	/**
+	 * tested with 1.1.1, but should work with other versions
+	 */
+	@XmlElement(required = true, name = "version")
+	private String version = "1.1.1";
+
+	/**
+	 * no spaces allowed, must be replaced with %20 in the url
+	 */
+	@XmlElement(required = true, name = "layers")
+	private String layers = "";
+
+	/**
+	 * currently only the coordinate system epsg:4326 is supported
+	 */
+	@XmlElement(required = true, name = "coordinatesystem", defaultValue = "EPSG:4326")
+	private String coordinatesystem = "EPSG:4326";
+
+	/**
+	 * some wms needs more parameters: &amp;EXCEPTIONS=BLANK&amp;Styles= .....
+	 */
+	@XmlElement(required = false, name = "aditionalparameters")
+	private String aditionalparameters = "";
+	
+	/**
+	 * a mapping list for zoom level to layers
+	 * each element looks like
+	 * <zoomMapping>
+	 * 		<zoomlevel>3,4,5,6</zoomlevel>
+	 * 		<layers>someLayerName,someOtherLayer</layers>
+	 * <zoomMapping>
+	 */
+	@XmlElementWrapper(name="layerMappings")
+	@XmlElements({ @XmlElement(name = "layerMapping", type = LayerMapping.class)})
+	private ArrayList<LayerMapping>layermappings=new ArrayList<LayerMapping>();
+	
+	private HashMap<Integer, String> zoomToLayer=new HashMap<Integer, String>();
+	protected void afterUnmarshal(Unmarshaller u, Object parent) {
+		for(LayerMapping z: layermappings){
+			String levels[]=z.zoom.split(" *, *");
+			for (String l: levels){
+				try {
+					int lvl=Integer.parseInt(l);
+					zoomToLayer.put(new Integer(lvl), z.layers);
+				}catch (NumberFormatException e){}
+			}
+		}
+		
+	}
+
+	@Override
+	public String getTileUrl(int zoom, int tilex, int tiley) {
+		double[] coords = MapSourceTools.calculateLatLon(this, zoom, tilex, tiley);
+		String clayers=layers;
+		String zlayers=zoomToLayer.get(new Integer(zoom));
+		if (zlayers != null){
+			clayers=zlayers;
+		}
+		String url = this.url + "REQUEST=GetMap" + "&LAYERS=" + clayers + "&SRS=" + coordinatesystem + "&VERSION="
+				+ version + "&FORMAT=image/" + tileType.getMimeType() + "&BBOX=" + coords[0] + "," + coords[1] + ","
+				+ coords[2] + "," + coords[3] + "&WIDTH=256&HEIGHT=256" + aditionalparameters;
+		return url;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public String getLayers() {
+		return layers;
+	}
+
+	public String getCoordinatesystem() {
+		return coordinatesystem;
+	}
+
+
+	
+
+}
