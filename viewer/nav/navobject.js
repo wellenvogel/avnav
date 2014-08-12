c710d22f48db5186807a89d8192f02bedfd871cb
wellenvogel <andreas@wellenvogel.net>
2014-08-12 21:57:21 +0200

    be robust against no gps time

--- viewer/nav/navobject.js
+++ viewer/nav/navobject.js
@@ -191,8 +191,9 @@ avnav.nav.NavObject.prototype.computeValues=function(){
     this.data.routeNumPoints=route.points.length;
     this.data.routeLen=this.routeHandler.computeLength(0);
     this.data.routeRemain=this.routeHandler.computeLength(-1)+this.data.markerDistance;
-    var routetime = gps.rtime.getTime();
-    if (vmgapp > 0) {
+    var routetime=0;
+    if (gps.rtime) routetime= gps.rtime.getTime();
+    if (vmgapp > 0 && routetime != 0) {
         routetime += this.data.routeRemain / vmgapp * 3600 * 1000; //time in ms
         var routeDate = new Date(Math.round(routetime));
         this.data.routeEta = routeDate;
