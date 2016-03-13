a51019aae93b22e31f96450ae47b1a3bba7b6c59
andreas <andreas@wellenvogel.de>
2016-03-13 18:51:30 +0100

    intermediate connect

--- server/wpa_control.py
+++ server/wpa_control.py
@@ -11,6 +11,9 @@ class WpaControl():
     self.wpaAddr=wpaAddr
     self.ownAddr=ownAddr
     self.socket=None
+  def __del__(self):
+    if self.socket is not None:
+      self.close()
   def checkWpa(self):
     return os.path.exists(self.wpaAddr)
   def open(self):
@@ -41,17 +44,24 @@ class WpaControl():
     self.checkOpen()
     self.socket.send(request)
 
-  def runSimpleScommand(self,command):
-    self.sendRequest(command.upper())
+  def runSimpleScommand(self,command,upper=True):
+    if upper:
+      self.sendRequest(command.upper())
+    else:
+      self.sendRequest(command)
     rt=self.receiveData()
     if rt.strip() != "OK":
-      raise Exception("command %s returned an error: %s"%(command,rt.strip()))
+      raise Exception("command '%s' returned an error: %s"%(command,rt.strip()))
     return True
 
   def runFreeCommand(self,command):
     self.sendRequest(command)
     return self.receiveData()
 
+  ''' convert some response that contains a table
+      into an arry of dict
+      e.g. scan_result
+      '''
   def tableToDict(self,table):
     if not type(table) is list:
       table=table.splitlines()
@@ -67,7 +77,17 @@ class WpaControl():
         ldict[headings[j]]=lvalues[j]
       rt.append(ldict)
     return rt
-
+  ''' convert a response that has name=value pairs into a dict
+  '''
+  def linesToDict(self,data):
+    if not type(data) is list:
+      data=data.splitlines()
+    rt={}
+    for l in data:
+      (n,v)=re.split(" *= *",l,1)
+      if n is not None and v is not None:
+        rt[n]=v.strip()
+    return rt
 
 
   def startScan(self):
@@ -75,8 +95,52 @@ class WpaControl():
   def scanResults(self):
     data=self.runFreeCommand("SCAN_RESULTS")
     return self.tableToDict(data)
-
-
+  def status(self):
+    data=self.runFreeCommand("STATUS_VERBOSE")
+    return self.linesToDict(data)
+  def listNetworks(self):
+    data=self.runFreeCommand("LIST_NETWORKS")
+    return self.tableToDict(data)
+  def addNetwork(self):
+    data=self.runFreeCommand("ADD_NETWORK")
+    if data.strip() == "FAIL":
+      raise Exception("unable to add network: "+data.strip())
+    return data.strip()
+  '''configure a network (number as string), param being a dict
+  '''
+  def configureNetwork(self,id,param):
+    for k in param.keys():
+      self.runSimpleScommand("SET_NETWORK %s %s \"%s\""%(id,k,param[k]),False)
+    return id
+  def enableNetwork(self,id):
+    self.runSimpleScommand("ENABLE_NETWORK %s"%(id),False)
+    return id
+  def disableNetwork(self,id):
+    self.runSimpleScommand("DISABLE_NETWORK %s"%(id),False)
+    return id
+  def removeNetwork(self,id):
+    self.runSimpleScommand("REMOVE_NETWORK %s"%(id),False)
+    return id
+  ''' add a new network, set the parameter and enable it
+      if a network with the same ssid already exists reconfigure this one
+  '''
+  def connect(self,param):
+    if param.get('ssid') is None:
+      raise Exception("missing parameter ssid for connect")
+    ssid=param.get('ssid')
+    availableNetworks=self.listNetworks()
+    id=None
+    for nw in availableNetworks:
+      if nw.get('ssid') is not None and nw.get('ssid') == ssid:
+        id=nw.get('network id')
+        break
+    if id is None:
+      id=self.addNetwork()
+    else:
+      self.disableNetwork(id)
+    self.configureNetwork(id,param)
+    self.enableNetwork(id)
+    return id
 
 
 
@@ -88,11 +152,45 @@ if __name__=="__main__":
   mode=sys.argv[3]
   rq=sys.argv[4]
   if mode == "simple":
+    ok=False
     if rq == "scan":
+      ok=True
       w.startScan()
     if rq == "scan_results":
+      ok=True
       print w.scanResults()
-    print "simple command %s ok"%(rq)
+    if rq == "status":
+      ok=True
+      print w.status()
+    if rq == "list_networks":
+      ok=True
+      print w.listNetworks()
+    if rq == "add_network":
+      ok=True
+      print w.addNetwork()
+    if rq== "configure_network":
+      ok=True
+      p={}
+      for i in range(6,len(sys.argv)):
+        (n,v)=sys.argv[i].split("=")
+        print "param "+n+"="+v
+        p[n]=v
+      w.configureNetwork(sys.argv[5],p)
+    if rq =="remove_network":
+      ok=True
+      w.removeNetwork(sys.argv[5])
+    if rq== "connect":
+      ok=True
+      p={}
+      for i in range(5,len(sys.argv)):
+        (n,v)=sys.argv[i].split("=")
+        print "param "+n+"="+v
+        p[n]=v
+      print w.connect(p)
+    if ok:
+      print "simple command %s ok"%(rq)
+    else:
+      raise Exception("unknown command "+rq)
   else:
     w.sendRequest(rq)
     rt=w.receiveData()
