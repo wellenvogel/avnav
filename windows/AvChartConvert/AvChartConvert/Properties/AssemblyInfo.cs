70ec7cfcb9f9b85b9878b9886ba3f1dd9de9ef46
wellenvogel <andreas@wellenvogel.net>
2016-08-06 14:23:15 +0200

    intermediate: upload to homepage

--- windows/AvChartConvert/AvChartConvert/Properties/AssemblyInfo.cs
+++ windows/AvChartConvert/AvChartConvert/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.08.05")]
-[assembly: AssemblyFileVersion("2016.08.05")]
+[assembly: AssemblyVersion("2016.08.06")]
+[assembly: AssemblyFileVersion("2016.08.06")]
