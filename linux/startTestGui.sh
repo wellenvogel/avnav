926b2dd3576716d2a620cf586bf767b962e4a37a
andreas <andreas@wellenvogel.de>
2016-09-05 23:00:34 +0200

    add -d option for debug

--- linux/startTestGui.sh
+++ linux/startTestGui.sh
@@ -6,5 +6,9 @@ CFG=avnav_server.xml
 RUNDIR=$pdir/../data_linux
 rm -f $RUNDIR/$CFG
 cp $pdir/$CFG $RUNDIR
-$PROG -b $RUNDIR
+vdir="release"
+if [ "$1" = "-d" ] ; then
+  vdir="debug"
+fi
+$PROG -b $RUNDIR -u viewer=$pdir/../viewer/build/$vdir
 
