cee15aad874e489bb7d7e0fffd21d2e66cafc1a6
andreas <andreas@wellenvogel.de>
2017-02-28 20:28:44 +0100

    updated demo

--- sync-hp.sh
+++ sync-hp.sh
@@ -6,7 +6,7 @@ rsync -rav --exclude="*bak" --exclude="*odg" --delete $BASE/docs/ $TARGET/docs
 rsync -rav --include="*png" --include="*PNG" --exclude="*" --delete $BASE/viewer/images/ $TARGET/viewer/images/
 rsync -rav --include="avnav_min.js" --include="loader.js" --include="avnav_viewer.html" --include="avnav_viewer.less" --exclude="*" --delete $BASE/viewer/ $TARGET/viewer
 
-for lib in jquery jscolor less movable-type ol311 rangeslider
+for lib in jquery movable-type ol3201
 do
 	rsync -rav --delete $BASE/libraries/$lib/* $TARGET/libraries/$lib/
 done
