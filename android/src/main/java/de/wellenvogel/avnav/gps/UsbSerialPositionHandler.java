05f7a69916b348ef9913f9532b439472af6da9e0
wellenvogel <andreas@wellenvogel.net>
2017-03-28 18:35:10 +0200

    1st implementation of usb serial handler

--- android/src/main/java/de/wellenvogel/avnav/gps/UsbSerialPositionHandler.java
+++ android/src/main/java/de/wellenvogel/avnav/gps/UsbSerialPositionHandler.java
@@ -2,12 +2,19 @@ package de.wellenvogel.avnav.gps;
 
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,27 +27,88 @@ public class UsbSerialPositionHandler extends SocketPositionHandler {
 
     static private class UsbSerialSocket extends AbstractSocket{
         UsbDevice dev;
+        UsbDeviceConnection connection;
         String baud;
+        UsbSerialDevice serialPort;
+        ArrayList<Byte> buffer=new ArrayList<Byte>();
+        Object bufferLock=new Object();
+
+        private void notifyWaiters(){
+            synchronized (bufferLock){
+                bufferLock.notifyAll();
+            }
+        }
+        final UsbSerialInterface.UsbReadCallback callback=new UsbSerialInterface.UsbReadCallback() {
+            @Override
+            public void onReceivedData(byte[] bytes) {
+                synchronized (bufferLock) {
+                    for (byte b : bytes) {
+                        buffer.add(b);
+                    }
+                    bufferLock.notifyAll();
+                }
+            }
+        };
         final static String PREFIX="AvnUsbSerial";
-        UsbSerialSocket(UsbDevice dev,String baud)
+        UsbSerialSocket(Context ctx,UsbDevice dev,String baud)
         {
             super();
             this.dev=dev;
             this.baud=baud;
+            UsbManager manager=(UsbManager) ctx.getSystemService(Context.USB_SERVICE);
+            connection=manager.openDevice(dev);
+            //TODO: handle open connection error
         }
         @Override
         public void connect() throws IOException {
+            buffer.clear();
+            notifyWaiters();
             AvnLog.i(PREFIX,"connect to "+dev.getDeviceName());
+            serialPort = UsbSerialDevice.createUsbSerialDevice(dev, connection);
+            if (serialPort != null) {
+                if (serialPort.open()) {
+                    serialPort.setBaudRate(Integer.parseInt(baud));
+                    serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                    serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                    serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                    serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                    serialPort.read(callback);
+                } else {
+                    throw new IOException(PREFIX + ": unable to open serial device " + dev.getDeviceName());
+                }
+            }
+            else{
+                throw new IOException(PREFIX + ": unable to open serial device " + dev.getDeviceName());
+            }
         }
 
         @Override
         public InputStream getInputStream() throws IOException {
-            return new ByteArrayInputStream(new byte[]{});
+            return new InputStream() {
+                @Override
+                public int read() throws IOException {
+                    while (true) {
+                        if (serialPort == null) return -1;
+                        synchronized (bufferLock) {
+                            if (buffer.size() > 0) return (int) buffer.remove(0);
+                            if (serialPort == null) return -1;
+                            try {
+                                bufferLock.wait();
+                            } catch (InterruptedException e) {
+                            }
+                        }
+                    }
+                }
+            };
         }
 
         @Override
         public void close() throws IOException {
             AvnLog.i(PREFIX,"close connection to "+dev.getDeviceName());
+            if (serialPort == null) return;
+            serialPort.close();
+            serialPort=null;
+            notifyWaiters();
         }
 
         @Override
@@ -50,7 +118,7 @@ public class UsbSerialPositionHandler extends SocketPositionHandler {
     }
 
     UsbSerialPositionHandler(Context ctx, UsbDevice device,String baud, Properties prop){
-        super("UsbSerialPositionHandler",ctx,new UsbSerialSocket(device,baud),prop);
+        super("UsbSerialPositionHandler",ctx,new UsbSerialSocket(ctx,device,baud),prop);
     }
 
     public static UsbDevice getDeviceForName(Context ctx,String name){
