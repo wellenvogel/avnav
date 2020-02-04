ccb1da0d95571ada0b15787de4a9f467b62998b1
andreas <andreas@wellenvogel.de>
2020-02-04 18:50:13 +0100

    doc for canboat, signalk

--- docs/index.php
+++ docs/index.php
@@ -2,10 +2,8 @@
 $script=__FILE__;
 $me=$_SERVER['REQUEST_URI'];
 $ifc="docs/beschreibung.html";
-$ifcpath=preg_replace("?[^/]*$?","",$ifc);
 $mepath=preg_replace('?/[^/]*$?','',preg_replace("&[?].*&","",$me));
 $mebase=preg_replace("&[?].*&","",$me);
-if (isset($_REQUEST['ifc'])) $ifc=$_REQUEST['ifc'];
 if (isset($me)){
   $base="<dummy x=\"$me\"/dummy><base href=\"".$mepath."/".$ifcpath."\"></base>";
   }
@@ -23,21 +21,39 @@ $navlist=array(
 );
 include "../../Templates/top.php";
 
+if (isset($_REQUEST['ifc'])) $ifc=$_REQUEST['ifc'];
+$ifc=preg_replace("&[^0-9a-zA-Z./_-]*&","",$ifc);
+$ifc=preg_replace("/\.\./","",$ifc);
+$ifcpath=preg_replace("?[^/]*$?","",$ifc);
+$ifc=dirname($script)."/".$ifc;
+
 function callback($match){
   global $mebase,$ifcpath;
-//print_r($match);
+ #print_r($match);
   if ($match[2]!=""){
     return $match[1].$match[2].$match[3].$match[4];   
   }
   if ($match[3]=="/"|| $match[3]=="."){
     return $match[1].$match[3].$match[4];
   }
-  return $match[1].$mebase."?ifc=".$ifcpath."/".$match[3].$match[4];
-  
+  return $match[1].$mebase."?ifc=".$ifcpath."/".$match[3].$match[4];  
+}
+function callbacksrc($match){
+  global $mebase,$ifcpath;
+  #print_r($match);
+  if ($match[2]!=""){
+    return $match[1].$match[2].$match[3].$match[4];   
+  }
+  if ($match[3]=="/"|| $match[3]=="."){
+    return $match[1].$match[3].$match[4];
+  }
+  return $match[1].$ifcpath."/".$match[3].$match[4];  
 }
 $f=fopen($ifc,"r");
 while ($line=fgets($f)){
-  print preg_replace_callback('&(href= *")(https?[:]//)?(.)([^"]*)&',"callback",$line);
+  $line=preg_replace_callback('&(href= *")(https?[:]//)?(.)([^"]*)&',"callback",$line);
+  $line=preg_replace_callback('&(src= *")(https?[:]//)?(.)([^"]*)&',"callbacksrc",$line);  
+  print $line;
 }
 
 include "../../Templates/body-bottom.php";
