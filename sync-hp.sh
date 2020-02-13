f3c18a495cf13a8ac3d066738367c48769097fc8
andreas <andreas@wellenvogel.de>
2020-02-13 21:36:15 +0100

    some preparations for gallery

--- sync-hp.sh
+++ sync-hp.sh
@@ -6,10 +6,11 @@ if [ "$1" != "" ] ; then
  SUB=$1
 fi
 set -x
-rsync -rav --no-links --exclude="*bak" --exclude="*odg" --exclude="*php" --delete $BASE/docs/ $TARGET/docs
+rsync -rav --no-links --include=.htaccess --exclude="*bak" --exclude="*odg" --exclude="*php" --delete $BASE/docs/ $TARGET/docs
 rsync -rav --include="*php" --exclude="*" $BASE/docs/ $TARGET
 rsync -rav --delete $BASE/viewer/build/$SUB/ $TARGET/viewern
 rsync -rav --delete $BASE/mobac/testsrc/build/libs/avnav-mapsources.zip $TARGET/downloads
+cp -p $BASE/docs/indexav.json $TARGET/index.json
 
 for lib in movable-type ol3201
 do
