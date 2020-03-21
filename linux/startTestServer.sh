061ae3ca92de8cd132669be3990c9c998e0b94c9
andreas <andreas@wellenvogel.de>
2020-03-21 10:18:24 +0100

    make avnav script use the default chart dir, allow to run in dev env

--- linux/startTestServer.sh
+++ linux/startTestServer.sh
@@ -1,14 +1,29 @@
 #! /bin/sh
 pdir=`dirname $0`
 pdir=`readlink -f $pdir`
-PROG=$pdir/../server/avnav_server.py
+PROG=$pdir/avnav
 CFG=avnav_server.xml
 RUNDIR=$pdir/../data_linux
 rm -f $RUNDIR/$CFG
-cp $pdir/$CFG $RUNDIR
 vdir="release"
-if [ "$1" = "-d" ] ; then
-  vdir="debug"
-fi
-$PROG -e -w $RUNDIR -c $HOME/AvNavCharts/out -u viewer=$pdir/../viewer/build/$vdir $RUNDIR/avnav_server.xml
+gui=0
+while getopts dg opt; do
+case $opt in
+  d)
+    vdir=debug
+    ;;
+  g)
+    gui=1
+    ;;
+  \?)
+    echo "invalid option $opt"
+    exit 1
+esac
+done
+shift $((OPTIND-1))  
 
+if [ $gui = 0 ]; then
+  exec $PROG -e -b $RUNDIR -c $HOME/AvNavCharts/out -v $pdir/../viewer/build/$vdir -t $pdir/avnav_server.xml -a $pdir/..
+else
+  exec $PROG -e -b $RUNDIR -g -v $pdir/../viewer/build/$vdir -t $pdir/avnav_server.xml -a $pdir/..
+fi
