746597121b6fcbd203428a3a38775a0d2284065f
andreas <andreas@wellenvogel.de>
2020-11-28 21:03:19 +0100

    prepare for feature info from ocharts

--- viewer/map/avnavchartsource.js
+++ viewer/map/avnavchartsource.js
@@ -36,6 +36,87 @@ import {XYZ as olXYZSource} from 'ol/source';
 import * as olTransforms  from 'ol/proj/transforms';
 import {Tile as olTileLayer} from 'ol/layer';
 
+//we use a bit a dirty hack here:
+//ol3 nicely shows a lower zoom if the tile cannot be loaded (i.e. has an error)
+//to avoid server round trips, we use a local image url
+//the more forward way would be to return undefined - but in this case
+//ol will NOT show the lower zoom tiles...
+const invalidUrl = 'data:image/png;base64,i';
+/**
+ * compute the tile url
+ * @param layerOptions the avnavOptions from the layer
+ * @param coord
+ * @returns {string|undefined|*}
+ */
+const layerUrlFunction=function (layerOptions,coord) {
+    if (!coord) return undefined;
+    let zxy = coord;
+    let z = zxy[0];
+    let x = zxy[1];
+    let y = zxy[2];
+    //y = -y - 1; //revert change back for ol6...change for ol3-151 - commit af319c259b349c86a4d164c42cc4eb5884f896fb
+
+    if (layerOptions.zoomLayerBoundings) {
+        let found = false;
+        if (!layerOptions.zoomLayerBoundings[z]) return invalidUrl;
+        for (let bindex in layerOptions.zoomLayerBoundings[z]) {
+            let zbounds = layerOptions.zoomLayerBoundings[z][bindex];
+            if (zbounds.minx <= x && zbounds.maxx >= x && zbounds.miny <= y && zbounds.maxy >= y) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return invalidUrl;
+        }
+    }
+    if (layerOptions.wms) {
+        //construct the WMS url
+        let grid = layerOptions.source.getTileGrid();
+        //taken from tilegrid.js:
+        //let origin = grid.getOrigin(z);
+        //the xyz source seems to have a very strange origin - x at -... but y at +...
+        //but we rely on the origin being ll
+        //not sure if this is correct for all projections...
+        let origin = [-20037508.342789244, -20037508.342789244]; //unfortunately the ol3 stuff does not export this...
+        let resolution = grid.getResolution(z);
+        let tileSize = grid.getTileSize(z);
+        y = (1 << z) - y - 1;
+        let minX = origin[0] + x * tileSize * resolution;
+        let minY = origin[1] + y * tileSize * resolution;
+        let maxX = minX + tileSize * resolution;
+        let maxY = minY + tileSize * resolution;
+        //now compute the bounding box
+        let converter = olTransforms.get("EPSG:3857", layerOptions.projection || "EPSG:4326");
+        let bbox = converter([minX, minY, maxX, maxY]);
+        let rturl = layerurl + "SERVICE=WMS&REQUEST=GetMap&FORMAT=image/png&WIDTH=" + tileSize + "&HEIGHT=" + tileSize + "&SRS=" + encodeURI(layerOptions.projection);
+        let k;
+        let layers;
+        if (layerOptions.wmslayermap[z]) layers = layerOptions.wmslayermap[z];
+        for (k in layerOptions.wmsparam) {
+            let v = layerOptions.wmsparam[k];
+            if (layers && (k == "LAYERS" || k == "layers")) {
+                v = layers;
+            }
+            rturl += "&" + k + "=" + v;
+        }
+        rturl += "&BBOX=" + bbox[0] + "," + bbox[1] + "," + bbox[2] + "," + bbox[3];
+        return rturl;
+    }
+    if (layerOptions.inversy) {
+        y = (1 << z) - y - 1
+    }
+    if (!layerOptions.replaceInUrl) {
+        let tileUrl = z + '/' + x + '/' + y + ".png";
+        if (layerOptions.encryptFunction) {
+            tileUrl = "##encrypt##" + tileUrl;
+        }
+        return layerOptions.layerurl + '/' + tileUrl;
+    }
+    else {
+        return layerOptions.layerurl.replace("{x}", x).replace("{y}", y).replace("{z}", z);
+    }
+}
 class AvnavChartSource extends ChartSourceBase{
     constructor(mapholer, chartEntry) {
         super(mapholer,chartEntry);
@@ -76,6 +157,17 @@ class AvnavChartSource extends ChartSourceBase{
         return parseFloat(elem.getAttribute(attr));
     }
 
+    encryptUrl(url){
+        if (!this.encryptFunction) {
+            return url;
+        }
+        else {
+            let encryptPart = url.replace(/.*##encrypt##/, "");
+            let basePart = url.replace(/##encrypt##.*/, "");
+            let finalSrc = basePart + this.encryptFunction(encryptPart);
+            return finalSrc;
+        }
+    }
 ;
     parseLayerlist(layerdata, baseurl) {
         let self = this;
@@ -84,6 +176,7 @@ class AvnavChartSource extends ChartSourceBase{
         let xmlDoc = Helper.parseXml(layerdata);
         Array.from(xmlDoc.getElementsByTagName('TileMap')).forEach(function (tm) {
             let rt = {};
+            rt.encryptFunction=self.encryptFunction;
             //complete tile map entry here
             rt.inversy = false;
             rt.wms = false;
@@ -176,9 +269,10 @@ class AvnavChartSource extends ChartSourceBase{
                 layerurl = baseurl + "/" + rt.url;
             }
             else layerurl = rt.url;
-            let replaceInUrl = false;
+            rt.layerurl=layerurl;
+            rt.replaceInUrl = false;
             if (layerurl.indexOf("{x}") >= 0 && layerurl.indexOf("{y}") >= 0 && layerurl.indexOf("{z}") >= 0) {
-                replaceInUrl = true;
+                rt.replaceInUrl = true;
             }
             rt.extent = olExtent.applyTransform(rt.layerExtent, self.mapholder.transformToMap);
             if (rt.wms) {
@@ -208,96 +302,16 @@ class AvnavChartSource extends ChartSourceBase{
                 rt.wmslayermap = layermap;
 
             }
-            //we use a bit a dirty hack here:
-            //ol3 nicely shows a lower zoom if the tile cannot be loaded (i.e. has an error)
-            //to avoid server round trips, we use a local image url
-            //the more forward way would be to return undefined - but in this case
-            //ol will NOT show the lower zoom tiles...
-            let invalidUrl = 'data:image/png;base64,i';
             let source = undefined;
             //as WMS support is broken in OL3 (as always ol3 tries to be more intelligent than everybody...)
             //we always use an XYZ layer but directly construct the WMS tiles...
             source = new olXYZSource({
-                tileUrlFunction: function (coord) {
-                    if (!coord) return undefined;
-                    let zxy = coord;
-                    let z = zxy[0];
-                    let x = zxy[1];
-                    let y = zxy[2];
-                    //y = -y - 1; //revert change back for ol6...change for ol3-151 - commit af319c259b349c86a4d164c42cc4eb5884f896fb
-
-                    if (rt.zoomLayerBoundings) {
-                        let found = false;
-                        if (!rt.zoomLayerBoundings[z]) return invalidUrl;
-                        for (let bindex in rt.zoomLayerBoundings[z]) {
-                            let zbounds = rt.zoomLayerBoundings[z][bindex];
-                            if (zbounds.minx <= x && zbounds.maxx >= x && zbounds.miny <= y && zbounds.maxy >= y) {
-                                found = true;
-                                break;
-                            }
-                        }
-                        if (!found) {
-                            return invalidUrl;
-                        }
-                    }
-                    if (rt.wms) {
-                        //construct the WMS url
-                        let grid = rt.source.getTileGrid();
-                        //taken from tilegrid.js:
-                        //let origin = grid.getOrigin(z);
-                        //the xyz source seems to have a very strange origin - x at -... but y at +...
-                        //but we rely on the origin being ll
-                        //not sure if this is correct for all projections...
-                        let origin = [-20037508.342789244, -20037508.342789244]; //unfortunately the ol3 stuff does not export this...
-                        let resolution = grid.getResolution(z);
-                        let tileSize = grid.getTileSize(z);
-                        y = (1 << z) - y - 1;
-                        let minX = origin[0] + x * tileSize * resolution;
-                        let minY = origin[1] + y * tileSize * resolution;
-                        let maxX = minX + tileSize * resolution;
-                        let maxY = minY + tileSize * resolution;
-                        //now compute the bounding box
-                        let converter = olTransforms.get("EPSG:3857", rt.projection || "EPSG:4326");
-                        let bbox = converter([minX, minY, maxX, maxY]);
-                        let rturl = layerurl + "SERVICE=WMS&REQUEST=GetMap&FORMAT=image/png&WIDTH=" + tileSize + "&HEIGHT=" + tileSize + "&SRS=" + encodeURI(rt.projection);
-                        let k;
-                        let layers;
-                        if (rt.wmslayermap[z]) layers = rt.wmslayermap[z];
-                        for (k in rt.wmsparam) {
-                            let v = rt.wmsparam[k];
-                            if (layers && (k == "LAYERS" || k == "layers")) {
-                                v = layers;
-                            }
-                            rturl += "&" + k + "=" + v;
-                        }
-                        rturl += "&BBOX=" + bbox[0] + "," + bbox[1] + "," + bbox[2] + "," + bbox[3];
-                        return rturl;
-                    }
-                    if (rt.inversy) {
-                        y = (1 << z) - y - 1
-                    }
-                    if (!replaceInUrl) {
-                        let tileUrl = z + '/' + x + '/' + y + ".png";
-                        if (self.encryptFunction) {
-                            tileUrl = "##encrypt##" + tileUrl;
-                        }
-                        return layerurl + '/' + tileUrl;
-                    }
-                    else {
-                        return layerurl.replace("{x}", x).replace("{y}", y).replace("{z}", z);
-                    }
-                },
+                tileUrlFunction:(coord)=>{
+                    return layerUrlFunction(rt,coord);
+                } ,
                 extent: rt.extent,
                 tileLoadFunction: function (imageTile, src) {
-                    if (!self.encryptFunction) {
-                        imageTile.getImage().src = src;
-                    }
-                    else {
-                        let encryptPart = src.replace(/.*##encrypt##/, "");
-                        let basePart = src.replace(/##encrypt##.*/, "");
-                        let finalSrc = basePart + self.encryptFunction(encryptPart);
-                        imageTile.getImage().src = finalSrc;
-                    }
+                    imageTile.getImage().src=self.encryptUrl(src);
                 }
 
                 /*
@@ -311,6 +325,7 @@ class AvnavChartSource extends ChartSourceBase{
             };
             if (self.chartEntry.opacity !== undefined) layerOptions.opacity=parseFloat(self.chartEntry.opacity);
             let layer = new olTileLayer(layerOptions);
+            rt.isTileLayer=true;
             layer.avnavOptions = rt;
             ll.push(layer);
         });
@@ -351,6 +366,72 @@ class AvnavChartSource extends ChartSourceBase{
     destroy() {
         super.destroy();
         this.destroySequence++;
+    }
+
+
+    getChartFeaturesAtPixel(pixel) {
+        return new Promise((resolve,reject)=>{
+            //if (! this.chartEntry.hasFeatureInfo) resolve([]);
+            if (! this.isReady()) resolve([]);
+            let mapcoordinates=this.mapholder.pixelToCoord(pixel);
+            let lonlat=this.mapholder.transformFromMap(mapcoordinates);
+            let offsetPoint=this.mapholder.pixelToCoord([pixel[0]+globalStore.getData(keys.properties.clickTolerance)/2,pixel[1]])
+            let offsetLatlon=this.mapholder.transformFromMap(offsetPoint);
+            let tolerance=Math.abs(offsetLatlon[0]-lonlat[0]);
+            let layerActions=[];
+            for (let i=this.layers.length-1;i>=0;i--){
+                let layer=this.layers[i];
+                if (! layer.getVisible()) continue;
+                if (! layer.avnavOptions) continue;
+                if (! layer.avnavOptions.isTileLayer) continue;
+                let zoom=Math.floor(this.mapholder.getView().getZoom());
+                let tile=layer.getSource().getTileGrid()
+                    .getTileCoordForCoordAndZ(mapcoordinates,zoom);
+                let url=layerUrlFunction(layer.avnavOptions,tile);
+                let action=new Promise((aresolve,areject)=>{
+                    let finalUrl=this.encryptUrl(url);
+                    Requests.getJson(finalUrl,{useNavUrl:false,noCache:false},{
+                        featureInfo:1,
+                        lat:lonlat[1],
+                        lon:lonlat[0],
+                        tolerance: tolerance
+                    })
+                        .then((result)=>{
+                            if (result.data && result.data !== "") {
+                                aresolve([result.data])
+                            }
+                            else{
+                                aresolve([]);
+                            }
+                        })
+                        .catch((error)=>aresolve([error])); //TODO
+                });
+                layerActions.push(action);
+            }
+            if (layerActions.length < 1) resolve([]);
+            Promise.all(layerActions)
+                .then((results)=>{
+                    let topInfo;
+                    for (let i=0;i<results.length;i++){
+                        if (results[i].length > 0){
+                            topInfo=results[i][0];
+                            break;
+                        }
+                    }
+                    if (topInfo) {
+                        let info={
+                            overlayType:'chart',
+                            overlayName:this.chartEntry.name,
+                            coordinates: lonlat,
+                            htmlInfo: topInfo
+                        }
+                        resolve([info]);
+                    }
+                    else resolve([]);
+                })
+                .catch((error)=>reject(error));
+
+        });
     }
 }
 
