ba170000803b6574a32862c585bb84733e62e0f3
andreas <andreas@wellenvogel.de>
2020-12-26 17:37:54 +0100

    disable fullscreen for chrome < 44

--- viewer/components/Fullscreen.js
+++ viewer/components/Fullscreen.js
@@ -2,11 +2,26 @@ import fullscreen from 'fullscreen-polyfill';
 import globalStore from '../util/globalstore.jsx';
 import keys from '../util/keys.jsx';
 
+let fullScreenBlocked=false;
+let userAgent=navigator.userAgent;
+//it seems that fullscreen does not work on older android versions (at least tested to work since 6/chrome44)
+if (userAgent.match(/Chrome/)){
+    let chromeVersion=userAgent.replace(/.*Chrome[/]*/,'').replace(/ .*/,'');
+    if (chromeVersion){
+        chromeVersion=chromeVersion.replace(/\..*/,'');
+        if (chromeVersion < 44){
+            fullScreenBlocked=true;
+        }
+    }
+}
+
 try {
-    globalStore.storeData(keys.gui.global.isFullScreen,!!document.fullscreenElement);
-    document.addEventListener('fullscreenchange', () => {
-        globalStore.storeData(keys.gui.global.isFullScreen,!!document.fullscreenElement);
-    });
+    globalStore.storeData(keys.gui.global.isFullScreen,! fullScreenBlocked && !!document.fullscreenElement);
+    if (! fullScreenBlocked) {
+        document.addEventListener('fullscreenchange', () => {
+            globalStore.storeData(keys.gui.global.isFullScreen, !!document.fullscreenElement);
+        });
+    }
 
 }catch (e){
     console.log("unable to install fullscreen handler")
@@ -14,6 +29,7 @@ try {
 
 
 const toggleFullscreen=()=>{
+    if (! fullScreenAvailable()) return;
     let element=document.fullscreenElement;
     if (! element) {
         document.body.requestFullscreen();
@@ -24,7 +40,7 @@ const toggleFullscreen=()=>{
 };
 
 const fullScreenAvailable=()=>{
-   return !!document.fullscreenEnabled
+   return ! fullScreenBlocked && !!document.fullscreenEnabled
 }
 
 const fullScreenDefinition={
