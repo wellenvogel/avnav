7f06febd245d35960809b2f0b07bb5db5d47d3b7
andreas <andreas@wellenvogel.de>
2021-03-02 20:39:44 +0100

    switch desk

--- raspberry/xui/start_firefox.sh
+++ raspberry/xui/start_firefox.sh
@@ -4,7 +4,7 @@ dbus-monitor --address "$DBUS_SESSION_BUS_ADDRESS" > /dev/null 2>&1 &
 monitor=$!
 while true
 do
- MOZ_USE_XINPUT2=1 firefox --profile /home/pi/.mozilla/firefox/avnav/ --class=AvNavFirefox 'http://localhost:8080?maximize=server'
+ MOZ_USE_XINPUT2=1 firefox --profile /home/pi/.mozilla/firefox/avnav/ --class=AvNavFirefox 'http://localhost:8080/viewer/avnav_viewer.html?fullscreen=server:desk2'
  kill -0 $monitor || exit 0
  sleep 1
 done
---
+++ raspberry/xui/switch_desk.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+DISPLAY=:0 xdotool key --clearmodifiers Super_L+$1
+
