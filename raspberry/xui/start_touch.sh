00f6a0a80f4b0e99cba6a168486a720d261677bd
andreas <andreas@wellenvogel.de>
2021-03-04 18:52:27 +0100

    first setup successfull

--- raspberry/xui/start_touch.sh
+++ raspberry/xui/start_touch.sh
@@ -4,6 +4,8 @@ CFG=/boot/avnav.conf
 
 X=/usr/lib/xorg/Xorg
 if [ "$1" = "prepare" ] ; then
+  logger -t avnavstartx preparing
+  echo "running prepare"
   if [ "$AVNAV_STARTX" != "yes" ] ; then
     exit 0
   fi
@@ -11,6 +13,8 @@ if [ "$1" = "prepare" ] ; then
     echo "$X not found, cannot start UI"
     exit 1
   fi
+  rm -f /tmp/.X0-lock
+  rm -f /tmp/.X11-unix/X0
   #workaround for modeset errors
   chmod u+s $X
   #tty
