b22f1f3645dae6d66e53c08e8cbd862140ce5df3
andreas <andreas@wellenvogel.de>
2021-03-12 16:01:29 +0100

    restart avahi when changing the system time

--- raspberry/settime.sh
+++ raspberry/settime.sh
@@ -3,3 +3,6 @@ sudo -n date -u "$*" || exit 1
 sudo -n systemctl mask ntp
 sudo -n /sbin/fake-hwclock save force
 sudo -n systemctl restart fake-hwclock
+#we must restart avahi as otherwise it will not respond any more
+sudo -n systemctl try-restart avahi-daemon
+exit 0
