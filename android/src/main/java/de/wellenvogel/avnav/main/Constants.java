9847610b563f80c048d1580152066baf43a936b3
andreas <andreas@wellenvogel.de>
2021-04-09 18:13:32 +0200

    remove unused stuff

--- android/src/main/java/de/wellenvogel/avnav/main/Constants.java
+++ android/src/main/java/de/wellenvogel/avnav/main/Constants.java
@@ -8,34 +8,13 @@ public class Constants {
     public static final String WORKDIR="workdir";
     public static final String CHARTDIR="chartdir";
     public static final String SHOWDEMO="showdemo";
-    public static final String INTERNALGPS="internalGps";
     public static final String EXTERNALACCESS="externalaccess";
     public static final String ALARMSOUNDS="alarmSounds";
     public static final String IPNMEA="ip.nmea";
     public static final String IPAIS="ip.ais";
     public static final String IPADDR="ip.addr";
     public static final String IPPORT="ip.port";
-    public static final String AISSENDPOS="ais.sendpos";
-    public static final String BTNMEA="bt.nmea";
-    public static final String BTAIS="bt.ais";
-    public static final String BTDEVICE="bt.device";
-    public static final String BTOFFSET="bt.offset";
-    public static final String USBDEVICE="usb.device";
-    public static final String USBNMEA="usb.nmea";
-    public static final String USBAIS="usb.ais";
-    public static final String USBBAUD="usb.baud";
-    public static final String IPCONNTIMEOUT="ip.conntimeout";
-    public static final String IPPOSAGE="ip.posAge";
-    public static final String AUXAGE="ip.auxAge";
-    public static final String AISLIFETIME ="ais.Lifetime";
     public static final String AISOWN="ais.own"; //own MMSI to filter
-    public static final String IPAISCLEANUPIV="ip.aisCleanupIv";
-    public static final String IPOFFSET="ip.offset";
-    public static final String GPSOFFSET="gps.offset";
-    public static final String NMEALOG="gps.logNmea";
-    public static final String AISLOG="gps.logAis";
-    public static final String NMEAFILTER="gps.filter";
-    public static final String NMEALOGFILTER="gps.logFilter";
     public static final String HIDE_BARS="layout.hideBars";
     public static final String RUNMODE="runmode"; //normal,server,xwalk
     public static final String WAITSTART="waitstart";
@@ -45,8 +24,6 @@ public class Constants {
     public static final String GPSALARM="alarm.gps";
     public static final String WAYPOINTALARM="alarm.waypoint";
     public static final String MOBALARM="alarm.mob";
-    public static final String GPS_PERMISSION_REQUESTED_NUM="permissions.numgps";
-    public static final String STORAGE_PERMISSION_REQUESTED_NUM="permissions.numstorage";
     public static final String ADDON_CONFIG="addon.config";
     //new handler config
     public static final String HANDLER_CONFIG="internal.handler";
@@ -68,32 +45,16 @@ public class Constants {
             MOBALARM
     };
     public static final String PREFNAME="AvNav";
-    //modes
-    public static final String MODE_NORMAL="normal";
-    public static final String MODE_XWALK="xwalk";
     public static final String MODE_SERVER ="server";
     public static final String LOGPRFX="avnav";
     //request codes in main activity
     public static final int SETTINGS_REQUEST=1;
     public static final int FILE_OPEN=100;
 
-    public static final String TRACKINTERVAL ="track.interval";
-    public static final String TRACKDISTANCE ="track.distance";
-    public static final String TRACKMINTIME ="track.mintime";
-    public static final String TRACKTIME ="track.time";
-
-    //modes
-    public static final String MODE_INTERNAL="internal";
-    public static final String MODE_IP="ip";
-    public static final String MODE_BLUETOOTH="bluetooth";
-    public static final String MODE_USB="usb";
-    public static final String MODE_NONE="none";
-
     public static final String BC_STOPALARM="de.wellenvogel.avnav.STOPALARM";
     public static final String BC_STOPAPPL="de.wellenvogel.avnav.STOPAPPL";
     public static final String BC_TRIGGER="de.wellenvogel.avnav.TRIGGER";
     public static final String BC_RELOAD_DATA ="de.wellenvogel.avnav.RELOAD_DATA";
-    public static final String BC_USB_ATTACH ="de.wellenvogel.avnav.USBATTACH";
 
     public static final int LOCALNOTIFY=1;
 
