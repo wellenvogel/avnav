1fef7bcc8cfd94842b74a51f63ee5021c7a0e360
pi <pi@pi-virtual-machine>
2021-05-11 22:54:54 +0200

    sailsteer configuration updated

--- viewer/util/keys.jsx
+++ viewer/util/keys.jsx
@@ -382,7 +382,7 @@ let keys={
         sailsteermarke: new Property(0,"Anlieger vom Wegpunkt anzeigen",PropertyType.CHECKBOX),///*an und gibt den Zielkurs zum Erreichen der Markierung bzw. des Wegpunktes an*/
         sailsteerrefresh: new Property(5,"Windreher Zeitraum [min] ",PropertyType.RANGE,[0,60]),
         //sailsteertide: new Property(0,"Berechnet den Gezeiteneffekt für das Boot basierend auf den COG-Daten und überträgt das Ergebnis auf die Anlieger",PropertyType.CHECKBOX),
-		sailsteeroverlap: new Property(0,"Erweitert die Anlieger über den Wende/Halse-Schnittpunkt hinaus",PropertyType.CHECKBOX),
+		sailsteeroverlap: new Property(0,"Anlieger über den Wende/Halse-Schnittpunkt hinaus erweitern",PropertyType.CHECKBOX),
         sailsteerlength: new Property(100,"Legt die Länge der Anlieger fest [nm]",PropertyType.RANGE,[0,10000]),
         //sailsteerlimits: new Property(2,"Legt die minimale und maximale Dauer der Wende/Halse fest [min]",PropertyType.RANGE,[0,30]),
 		sailsteertransparency: new Property(20,"Transparenz des Sailsteerdisplays [%]",PropertyType.RANGE,[0,100]),
