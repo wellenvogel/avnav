007431173e147f3467935b071a3d8c54b04a8e70
andreas <andreas@wellenvogel.de>
2022-02-10 20:15:45 +0100

    #182: prevent setting empty formatter parameters if they are not defined in the layout

--- viewer/components/WidgetFactory.jsx
+++ viewer/components/WidgetFactory.jsx
@@ -8,7 +8,6 @@ import Formatter from '../util/formatter';
 import Visible from '../hoc/Visible.jsx';
 import ExternalWidget from './ExternalWidget.jsx';
 import keys,{KeyHelper} from '../util/keys.jsx';
-import Formatters from '../util/formatter';
 import Requests from '../util/requests.js';
 import base from '../base.js';
 import {GaugeRadial,GaugeLinear} from './CanvasGauges.jsx';
@@ -18,6 +17,7 @@ export const filterByEditables=(editableParameters,values)=>{
     let rt={};
     if (! editableParameters) return rt;
     editableParameters.forEach((param)=>{
+        if ( ! param.name in values) return;
         let v=param.getValue(values);
         param.setValue(rt,v);
     });
@@ -108,7 +108,7 @@ class FormatterParamWidgetParameter extends EditableParameter {
     }
     getValue(widget){
         let rt=widget[this.name];
-        if (! rt) return [];
+        if (! rt) return;
         if (typeof(rt) === 'string') return rt.split(",");
         return rt;
     }
@@ -128,7 +128,7 @@ class FormatterParamWidgetParameter extends EditableParameter {
         let formatter=widget.formatter;
         if (formatter){
             if (typeof(formatter) !== 'function'){
-                formatter = Formatters[formatter];
+                formatter = Formatter[formatter];
             }
             if (formatter && formatter.parameters){
                 parameterDescriptions=formatter.parameters;
