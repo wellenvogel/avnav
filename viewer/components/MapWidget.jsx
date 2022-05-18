cfc8626e05fb8067634b3885125160b889f660cd
wellenvogel <andreas@wellenvogel.de>
2022-05-18 18:45:54 +0200

    add some distance/course computation functions to the js api

--- viewer/components/MapWidget.jsx
+++ viewer/components/MapWidget.jsx
@@ -20,34 +20,67 @@ class MapWidget extends React.Component{
     initUserContext(){
         let self=this;
         this.userContext={};
+        /**
+         * convert lon, lat to canvas pixel
+         * @param lon
+         * @param lat
+         * @returns {number[]|*} [x,y] pixel (starting upper left)
+         */
         this.userContext.lonLatToPixel=(lon,lat)=>{
             if (! this.drawing) return[0,0];
             let mapcoord=this.drawing.pointToMap([lon,lat]);
             return this.drawing.pixelToDevice(this.drawing.pointToCssPixel(mapcoord));
         }
+        /**
+         * invers to #lonLatToPixel
+         * @param x
+         * @param y
+         * @returns {number[]|*} [lon,lat]
+         */
         this.userContext.pixelToLonLat=(x,y)=>{
             if (! this.drawing) return [0,0];
             x=x/this.drawing.getDevPixelRatio();
             y=y/this.drawing.getDevPixelRatio();
             return this.drawing.pointFromMap(this.drawing.cssPixelToCoord([x,y]));
         }
+        /**
+         * get the scale factor for high dpi displays
+         * 1 is the base
+         * @returns {number|*|number|number}
+         */
         this.userContext.getScale=()=>{
             if (! this.drawing) return 1;
             return this.drawing.useHdpi?this.drawing.devPixelRatio:1;
         }
+        /**
+         * get the map rotation
+         * @returns {number|*} rotation in rad!!!
+         */
         this.userContext.getRotation=()=>{
             if (! this.drawing) return 0;
             return this.drawing.getRotation();
         }
+        /**
+         * get the drawing context
+         * @returns {*}
+         */
         this.userContext.getContext=()=>{
             if (! this.drawing) return;
             return this.drawing.getContext();
         }
+        /**
+         * get the canvas size
+         * @returns {number[]} [width,height]
+         */
         this.userContext.getDimensions=()=>{
             if (! this.drawing) return [0,0];
             let cv=this.drawing.getContext().canvas;
             return [cv.width*this.drawing.getDevPixelRatio(),cv.height*this.drawing.getDevPixelRatio()];
         }
+        /**
+         * function to trigger a new render of the map
+         * this function can also be called outside the renderCanvas function - e.g. from a timer
+         */
         this.userContext.triggerRender=()=>{
             window.setTimeout(()=>self.props.triggerRender(),0);
         }
