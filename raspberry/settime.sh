06f1cc91442b9819f362c4d18515e018898a9932
andreas <andreas@wellenvogel.de>
2023-01-18 15:39:57 +0100

    restart wlan-av1 when changing the system time

--- raspberry/settime.sh
+++ raspberry/settime.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
+if [ "$1" != "" ] ; then
 sudo -n date -u "$*" || exit 1
+fi
 if [ $(sudo systemctl is-enabled ntp) != "masked" ]
 then
   sudo -n systemctl mask ntp
@@ -8,4 +10,5 @@ sudo -n /sbin/fake-hwclock save force
 sudo -n systemctl restart fake-hwclock
 #we must restart avahi as otherwise it will not respond any more
 sudo -n systemctl try-restart avahi-daemon
+(echo restart wlan-av1; sudo ifdown wlan-av1; sleep 10; echo start wlan-av1;sudo ifup wlan-av1) &
 exit 0
