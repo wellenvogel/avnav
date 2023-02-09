af1fe9e4c4d74e5d55e3b687e9878b276226b028
andreas <andreas@wellenvogel.de>
2023-02-09 17:39:34 +0100

    add default settings to local start url

--- raspberry/xui/start_firefox.sh
+++ raspberry/xui/start_firefox.sh
@@ -12,7 +12,7 @@ do
    mkdir -p $PROFILE && cp -rp /usr/lib/avnav/raspberry/xui/firefox-profile/* $PROFILE
  fi 
  #we assume that we maybe have a nice dimm command available on the server
- MOZ_USE_XINPUT2=1 firefox --profile $PROFILE --class=AvNavFirefox 'http://localhost:8080/viewer/avnav_viewer.html?fullscreen=server:desk2&dimm=server:dimm'
+ MOZ_USE_XINPUT2=1 firefox --profile $PROFILE --class=AvNavFirefox 'http://localhost:8080/viewer/avnav_viewer.html?fullscreen=server:desk2&dimm=server:dimm&defaultSettings=.*localFirefox'
  kill -0 $monitor || exit 0
  sleep 1
 done
