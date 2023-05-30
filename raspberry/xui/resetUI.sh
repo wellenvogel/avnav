c5cca8bf7324971a07ce8981102fc6b00102a972
wellenvogel <andreas@wellenvogel.de>
2023-05-30 22:23:41 +0200

    be more verbose when resetting UI

--- raspberry/xui/resetUI.sh
+++ raspberry/xui/resetUI.sh
@@ -2,6 +2,11 @@
 PROFILE=/home/pi/.mozilla/firefox/avnav
 DELETE_MARKER=AVNAV_DELETE
 if [ -d "$PROFILE" ] ; then
+    echo "preparing deletion of $PROFILE"
     touch "$PROFILE/$DELETE_MARKER"
+    if [ $? != 0 ] ; then
+      echo "unable to write $PROFILE/$DELETE_MARKER"
+      exit 1
+    fi
 fi
 sudo -n systemctl restart avnav-startx
