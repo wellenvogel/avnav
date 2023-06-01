4a61a9dec320108e488dd24c8f035aa4b03d30ce
wellenvogel <andreas@wellenvogel.de>
2023-06-01 20:29:47 +0200

    add python3-xlib

--- raspberry/xui/setup.sh
+++ raspberry/xui/setup.sh
@@ -48,7 +48,7 @@ apt-get install python-xdg || apt-get install python3-xdg || err "unable to inst
 apt-get install -y --no-install-recommends xserver-xorg-video-all \
   xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils \
   onboard  at-spi2-core onboard-data mousetweaks gir1.2-ayatanaappindicator3-0.1 gir1.2-atspi-2.0 \
-  openbox lxterminal dconf-cli firefox-esr dbus-x11 \
+  openbox lxterminal dconf-cli firefox-esr dbus-x11 python3-xlib \
   nemo xfce4-panel mousepad xdotool menu libglib2.0-bin || err "unable to install"
 
 copyNE /boot avnav.conf 
