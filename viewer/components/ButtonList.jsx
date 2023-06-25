40c9aa3f0bd10e880144a7009d8977244f69f5a0
andreas <andreas@wellenvogel.de>
2023-06-25 20:35:53 +0200

    #283: correctly resize buttons also in 2 column mode

--- viewer/components/ButtonList.jsx
+++ viewer/components/ButtonList.jsx
@@ -96,6 +96,7 @@ class ButtonList extends React.Component{
         //we must render all invsible buttons to be sure to get called back
         //if their visibility changes
         let invisibleItems=[];
+        let allowedOverflowItems=0;
         for (let k in this.props.itemList){
             let stateKey=this.getStateKey(this.props.itemList[k]);
             if (!stateKey) continue;
@@ -105,46 +106,57 @@ class ButtonList extends React.Component{
             else{
                 invisibleItems.push(this.props.itemList[k]);
             }
+            if (this.props.itemList[k].overflow) allowedOverflowItems++;
         }
         items=this.itemSort(items);
-        let maxButtons=this.props.maxButtons;
-        if (this.props.buttonCols) maxButtons=Math.ceil(maxButtons/2);
         let scale=1;
         let hasOverflow=false;
-        if (listHeight && this.props.buttonHeight){
-            scale=listHeight/(this.props.buttonHeight*maxButtons);
-            if (scale > 1) scale=1;
-            if (items.length*this.props.buttonHeight*scale > listHeight) {
-                hasOverflow=true;
+        /* strategy:
+           add 1 to len for overflow button (cannot overflow) if buttonCols is not set
+           check if we can overflow without scaling - overflow items will be at most == remaining
+           compute scale based on remaining
+         */
+        let moveToOverflow=0;
+        if (listHeight && this.props.buttonHeight) {
+            let completeLength = items.length;
+            if (completeLength * this.props.buttonHeight > listHeight) {
+                if (! this.props.buttonCols) completeLength += 1; //overflow button
+                if (allowedOverflowItems > completeLength / 2) {
+                    allowedOverflowItems = Math.floor(completeLength / 2);
+                }
+                while (moveToOverflow < allowedOverflowItems && ((completeLength - moveToOverflow) * this.props.buttonHeight > listHeight)) {
+                    moveToOverflow++;
+                }
+                scale = listHeight / (this.props.buttonHeight * (completeLength - moveToOverflow));
             }
+            if (scale > 1) scale = 1;
         }
         let fontSize=this.props.buttonSize*scale/4.0;
         let mainItems=[];
         let overflowItems=[];
-        if (hasOverflow && !this.props.buttonCols){
-            //split the buttons into multiple lists
-            let maxInMain=Math.floor(listHeight/(this.props.buttonHeight*scale))-1;
-            let inOverflow=items.length-maxInMain;
-            for (let k=items.length-1;k>=0;k--){
-                if (items[k].overflow && inOverflow){
-                    overflowItems.splice(0,0,items[k]);
-                    inOverflow--;
-                }
-                else{
-                    mainItems.splice(0,0,items[k]);
-                }
-            }
-            if (overflowItems.length > 0) {
-                mainItems.push({
-                    name: 'Overflow',
-                    toggle: this.state.showOverflow,
-                    onClick: (el)=> {
-                        this.setState({showOverflow: !this.state.showOverflow})
+        if (moveToOverflow > 0){
+            hasOverflow=true;
+            if (! this.props.buttonCols) {
+                //split the buttons into multiple lists
+                for (let k = items.length - 1; k >= 0; k--) {
+                    if (items[k].overflow && moveToOverflow > 0) {
+                        overflowItems.splice(0, 0, items[k]);
+                        moveToOverflow--;
+                    } else {
+                        mainItems.splice(0, 0, items[k]);
                     }
-                });
-            }
-            else {
-                hasOverflow = false;
+                }
+                if (overflowItems.length > 0) {
+                    mainItems.push({
+                        name: 'Overflow',
+                        toggle: this.state.showOverflow,
+                        onClick: (el) => {
+                            this.setState({showOverflow: !this.state.showOverflow})
+                        }
+                    });
+                } else {
+                    hasOverflow = false;
+                }
             }
         }
         if (hasOverflow && !this.props.buttonCols){
