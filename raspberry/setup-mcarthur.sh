9a97469ce31350b19fab1ffa3b00e7a6ed73cd4a
free-x <oroitburd@gmail.com>
2023-07-19 14:42:44 +0200

    Add setup for McArthur HAT

---
+++ raspberry/setup-mcarthur.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+#set up Openmarine McArthur HAT (https://github.com/OpenMarine/MacArthur-HAT)
+#return 0 if ok, 1 if needs reboot, -1 on error 
+#set -x
+#testing
+. "$AVNAV_SETUP_HELPER"
+BASE=/
+WS_COMMENT="#MCARTHUR_DO_NOT_DELETE"
+#/boot/config.txt
+read -r -d '' CFGPAR <<'CFGPAR'
+dtparam=i2c_arm=on
+dtparam=spi=on
+dtoverlay=mcp251xfd,spi0-1,oscillator=20000000,interrupt=25
+dtoverlay=w1-gpio,gpiopin=19
+dtoverlay=uart3
+dtoverlay=uart5
+CFGPAR
+
+#/etc/network/interfaces.d/can0
+read -r -d '' CAN0 << 'CAN0'
+#physical can interfaces
+auto can0
+iface can0 can static
+bitrate 250000
+pre-up /sbin/ip link set $IFACE type can restart-ms 100
+down /sbin/ip link set $IFACE down
+up /sbin/ifconfig $IFACE txqueuelen 10000
+CAN0
+
+can="$BASE/etc/network/interfaces.d/can0"
+needsReboot=0
+if [ "$1" = $MODE_DIS ]; then
+  removeConfig "$BOOTCONFIG" "$WS_COMMENT"
+  checkRes
+  exit $needsReboot
+fi
+
+if [ "$1" = $MODE_EN ] ; then
+  checkConfig "$BOOTCONFIG" "$WS_COMMENT" "$CFGPAR"
+  checkRes
+  replaceConfig "$can" "$CAN0"
+  checkRes
+  cat /home/pi/.signalk/settings.json | jq '.pipedProviders += [{ "enabled": "true", "id": "MCARTHUR_SEATALK", "pipeElements": [{ "options" : { "logging" : false, "subOptions" : { "gpio": "GPIO20", "gpioInvert": false }, "type": "Seatalk" }, "type": "providers/simple"}] }]' > /tmp/settings.json
+  mv /tmp/settings.json /home/pi/.signalk/settings.json
+  log "needReboot=$needsReboot"
+  exit $needsReboot
+fi
+
+
+
+exit 0
