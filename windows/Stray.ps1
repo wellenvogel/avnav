21dcd38e7ac42e9da1c4a7777576f12191992d71
andreas <andreas@wellenvogel.de>
2024-05-12 19:43:50 +0200

    intermediate, not working: start avnav on windows

--- windows/Stray.ps1
+++ windows/Stray.ps1
@@ -66,6 +66,8 @@ function Kill-Tree {
 }
 
 $serverProcess = $null
+$serverStdoutReader=$null
+$serverStderrReader=$null
 
 function Set-Enable{
     $isInstalled=Test-Path -Path "$softwareBase"
@@ -91,9 +93,92 @@ function Set-Enable{
 }
 
 function Run-Server {
-    $exename = Join-Path $PSScriptRoot '\test.cmd'
+    try{
+        if ($null -ne $serverStdoutReader){
+            Unregister-Event $serverStdoutReader.Id
+            $serverStdoutReader=$null
+        }
+    }catch {
+
+    }
+    try{
+        if ($null -ne $serverStderrReader){
+            Unregister-Event $serverStderrReader.Id
+            $serverStderrReader=$null
+        }
+    }catch{
+
+    }
+    try{
+    $exename = Join-Path "$softwareBase" "python\python.exe"
+    if (-Not (Test-Path -Path "$exename")){
+        throw "python at $exename not found"
+    }
+    $avnav = Join-Path "$softwareBase" "server\avnav_server.py"
+    if (-Not (Test-Path -Path "$avnav")){
+        throw "Avnav main at $avnav not found"
+    }
+    $xml= Join-Path "$dataDir" "avnav_server.xml"
+    if (-Not (Test-Path -Path "$xml")){
+        #create from template
+        $template=Join-Path $PSScriptRoot "avnav_server.xml"
+        if (-Not (Test-Path -Path $template )){
+            throw "not config template at $template found"
+        }
+        if (-Not (Test-Path -Path "$dataDir")){
+            mkdir -Force "$dataDir"
+        }
+        if (-Not (Test-Path -Path "$dataDir")){
+            throw "unable to create data directory $dataDir"
+        }
+        Copy-Item "$template" -Destination "$xml"
+    }
+    $gdalpath=Join-Path "$softwareBase" "gdal\PFiles\GDAL"
+    if (-Not (Test-Path -Path "$gdalpath")){
+        throw "gdal not found at $gdalpath"
+    }
+    $gdaldata=Join-Path "$gdalpath" "gdal-data"
+    $gdalpython=Join-Path "$softwareBase" "gdal\Lib\site-packages"
+    $env=@{
+        GDAL_DATA = "$gdaldata"
+        PYTHONPATH = "$gdalpython"
+        PATH="$gdalpath"
+    }
     $null=md -Force $logDir
-    $global:serverProcess = Start-Process -FilePath "$exename" -PassThru -RedirectStandardOutput "$serverLog" -RedirectStandardError  "$serverError" -WindowStyle Hidden
+    $arglist=@("$avnav",'-q','-w',"$dataDir")
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName="$exename"
+    $startInfo.Arguments=$arglist
+    $startInfo.RedirectStandardError=$true
+    $startInfo.RedirectStandardOutput=$true
+    $startInfo.UseShellExecute=$false
+    $startInfo.EnvironmentVariables.Remove("GDAL_DATA")
+    $startInfo.EnvironmentVariables.Add("GDAL_DATA",$gdaldata)
+    $startInfo.EnvironmentVariables.Remove("PYTHONPATH")
+    $startInfo.EnvironmentVariables.Add("PYTHONPATH",$gdalpython)
+    $path=$gdalpath
+    if ($startInfo.EnvironmentVariables.ContainsKey('PATH')){
+        $path=$path+";"+$startInfo.EnvironmentVariables['PATH']
+    }
+    $startInfo.EnvironmentVariables.Remove('PATH')
+    $startInfo.EnvironmentVariables.Add('PATH',$path)
+    $proc=New-Object System.Diagnostics.Process
+    $proc.StartInfo=$startInfo
+    $global:serverStdoutReader= Register-ObjectEvent $proc -EventName OutputDataReceived {
+        Add-Content -Path $serverLog "$Event.SourceEventArgs.Data"
+    }
+    $global:serverStderrReader=Register-ObjectEvent $proc -EventName ErrorDataReceived {
+        Add-Content -Path $serverError "$Event.SourceEventArgs.Data"
+    }
+    $proc.Start()
+    $proc.BeginErrorReadLine()
+    $proc.BeginOutputReadLine()
+    $global:serverProcess = $proc
+    }catch{
+        Show-InputDialog -WindowTitle "StartupError" -Message $_.Exception.Message -ShowText $false
+        return $false
+    }
+    return $true
 }
 
 function Run-Installer([string]$url){
