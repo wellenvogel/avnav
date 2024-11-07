7229915de438df15774f2283abf94c30711beb56
andreas <andreas@wellenvogel.de>
2024-11-07 19:21:43 +0100

    remove eslint/compat to handle version issues

--- viewer/eslint.config.mjs
+++ viewer/eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
+//import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
@@ -11,5 +11,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  //...fixupConfigRules(pluginReactConfig),
 ];
