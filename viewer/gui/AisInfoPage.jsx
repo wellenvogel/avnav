a394e442e788b7607852c33231e10ccbbaad90fe
andreas <andreas@wellenvogel.de>
2025-05-10 20:37:46 +0200

    separate AIS info display into own component

--- viewer/gui/AisInfoPage.jsx
+++ viewer/gui/AisInfoPage.jsx
@@ -2,90 +2,16 @@
  * Created by andreas on 02.05.14.
  */
 
-import Dynamic from '../hoc/Dynamic.jsx';
-import ItemList from '../components/ItemList.jsx';
-import globalStore from '../util/globalstore.jsx';
-import keys from '../util/keys.jsx';
 import React from 'react';
 import Page from '../components/Page.jsx';
-import AisFormatter from '../nav/aisformatter.jsx';
 import MapHolder from '../map/mapholder.js';
 import GuiHelpers from '../util/GuiHelpers.js';
 import Mob from '../components/Mob.js';
-import {Drawing} from '../map/drawing.js';
 import MapEventGuard from "../hoc/MapEventGuard";
 import NavData from '../nav/navdata';
+import {ShowAisItemInfo, storeKeys} from "../components/AisInfoDisplay";
 
-
-const displayItems = [
-    {name: 'mmsi'},
-    {name: 'shipname'},
-    {name: 'callsign'},
-    {name: 'shiptype'},
-    {name: 'aid_type'},
-    {name: 'clazz'},
-    {name: 'status'},
-    {name: 'destination'},
-    {name: 'position'},
-    {name: 'headingTo'},
-    {name: 'distance'},
-    {name: 'course'},
-    {name: 'speed'},
-    {name: 'heading'},
-    {name: 'turn'},
-    {name: 'cpa'},
-    {name: 'tcpa'},
-    {name: 'bcpa'},
-    {name: 'passFront', addClass: 'aisFront'},
-    {name: 'length'},
-    {name: 'beam'},
-    {name: 'draught'},
-    {name: 'age'},
-];
-
-const createUpdateFunction=(config,mmsi)=>{
-    return (state)=>{
-        if (!mmsi) return {current:undefined,...config};
-        return {current:NavData.getAisHandler().getAisByMmsi(mmsi),...config};
-    }
-};
-const storeKeys={
-    aisSequence:keys.nav.ais.updateCount
-};
-const createItem=(config,mmsi)=>{
-    let cl="aisData";
-    if (config.addClass)cl+=" "+config.addClass;
-    return Dynamic((props)=> {
-        let key = props.name;
-        if (! AisFormatter.shouldShow(key,props.current)){
-            return null;
-        }
-        let target = props.current;
-        if (typeof(target) == 'undefined') { return null; }
-        let unit = AisFormatter.getUnit(props.name);
-        let clazz = 'aisInfoRow';
-        let warning = target.warning && (key.includes('cpa') || key.includes('pass'));
-        let warningDist = globalStore.getData(keys.properties.aisWarningCpa);
-        let warningTime = globalStore.getData(keys.properties.aisWarningTpa);
-        let hideAge = globalStore.getData(keys.properties.aisLostTime)
-        if((key.includes('pass') && warning)
-            || (0 < target.tcpa && target.cpa < warningDist && (key=='cpa' || (key=='tcpa' && target.tcpa < warningTime)))
-            || (key === 'age' && target.age > hideAge )
-        ){
-          clazz += ' warning';
-        }
-        return (
-          <div className={clazz}>
-              <div className='label'>{AisFormatter.getHeadline(key)}</div>
-              <div className={cl}>{AisFormatter.format(key, props.current)}{unit && <span className='unit'>&thinsp;{unit}</span>}</div>
-          </div>
-        );
-    },{
-        storeKeys:storeKeys,
-        updateFunction:createUpdateFunction(config,mmsi)
-    });
-};
-const GuardedList=MapEventGuard(ItemList);
+const GuardedInfo=MapEventGuard(ShowAisItemInfo);
 class AisInfoPage extends React.Component{
     constructor(props){
         super(props);
@@ -151,7 +77,6 @@ class AisInfoPage extends React.Component{
             }
         ];
         this.checkNoTarget=this.checkNoTarget.bind(this);
-        this.drawIcon=this.drawIcon.bind(this);
         this.timer=GuiHelpers.lifecycleTimer(this,this.checkNoTarget,5000,true);
     }
 
@@ -167,55 +92,19 @@ class AisInfoPage extends React.Component{
         }
         this.timer.startTimer(timerSequence);
     }
-    drawIcon(canvas,current){
-        if (! canvas) return;
-        if (! current) return;
-        let drawing=new Drawing({
-            coordToPixel:(p)=>{return p;},
-            pixelToCoord:(p)=>{return p;}
-        },globalStore.getData(keys.properties.style.useHdpi,false));
-        let ctx=canvas.getContext('2d');
-        drawing.setContext(ctx);
-        let rect=canvas.getBoundingClientRect();
-        canvas.width=rect.width;
-        canvas.height=rect.height;
-        let [style,symbol,scale]=MapHolder.aislayer.getStyleEntry(current);
-        drawing.drawImageToContext([rect.width/2,rect.height/2],symbol.image,style);
-        if (globalStore.getData(keys.properties.aisUseCourseVector) && current.course !== undefined){
-            let rd=Math.PI*current.course/180.0;
-            let ty = rect.height/2-Math.cos(rd)*rect.height;
-            let tx= rect.width/2+Math.sin(rd)*rect.width;
-            drawing.drawLineToContext([[rect.width/2,rect.height/2],[tx,ty]],{...style,color:style.courseVectorColor, width: globalStore.getData(keys.properties.navCircleWidth)})
-        }
-    }
 
     render(){
-        const Status = (props)=> {
-            return <canvas className="status" ref={(ctx)=>{this.drawIcon(ctx,props.current)}}/>
-        };
-        const RenderStatus=Dynamic(Status);
         //gets mmsi
         const MainContent=(props)=> {
-            return(
-            <React.Fragment>
-                <RenderStatus
-                    storeKeys={storeKeys}
-                    updateFunction={createUpdateFunction({},props.mmsi)}
-                    />
-                <GuardedList
-                    itemCreator={(config)=>{return createItem(config,props.mmsi)}}
-                    itemList={displayItems}
-                    scrollable={true}
-                    className="infoList"
-                    onClick={()=>{
+            return (
+                    <GuardedInfo
+                        {...props}
+                        onClick={()=>{
                         if (! this.props.history.backFromReplace()) {
                             this.props.history.pop();
-                        }
-                    }}
+                        }}}
                     />
-
-            </React.Fragment>
-            );
+                );
         };
 
         return (
