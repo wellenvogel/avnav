65084286b7cb8ba7a4eb8b7ebb841e15c495c1a2
andreas <andreas@wellenvogel.de>
2025-06-02 12:04:19 +0200

    handle weight by separate container on dashboard pages

--- viewer/gui/GpsPage.jsx
+++ viewer/gui/GpsPage.jsx
@@ -39,7 +39,10 @@ const widgetCreator=(nameIdx,weightSum,panelData)=>{
     let {weight,...widgetProps}=widget;
     if (weight === undefined) weight=1;
     let height=weight/weightSum*100;
-    return WidgetFactory.createWidget(widget,{style:{height:height+"%"},mode:'gps'});
+    const Widget=WidgetFactory.createWidget(widget,{mode:'gps'});
+    return (props)=><div className={'widgetWeight'} style={{height:height+"%"}}>
+        <Widget {...props}/>
+    </div>
 };
 
 const getLayoutPage=(pageNum)=>{
