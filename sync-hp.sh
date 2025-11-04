ad253ccc745b038c9e56a606c95353294021aa15
wellenvogel <andreas@wellenvogel.de>
2025-11-04 18:12:16 +0100

    sync_hp with rsync ssh

--- sync-hp.sh
+++ sync-hp.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 BASE=`dirname $0`
-TARGET=/diskstation/web/homepage/software/avnav
+TARGET="andreas@10.222.10.13:/volume1/web/homepage/software/avnav"
+#TARGET=/diskstation/web/homepage/software/avnav
 SUB=release
 if [ "$1" != "" ] ; then
  SUB=$1
 fi
 set -x
-rsync -rav --no-links --include=.htaccess --exclude-from="$BASE/exclude.sync" --delete $BASE/docs/ $TARGET/docs
-rsync -rav --no-links --include=.htaccess --exclude-from="$BASE/exclude.sync" $BASE/docs/downloads/ $TARGET/downloads
-rsync -rav --include="*php" --exclude="*" $BASE/docs/ $TARGET
-rsync -rav --delete $BASE/viewer/build/$SUB/ $TARGET/viewern
-rsync -rav --delete $BASE/viewer/demo/ $TARGET/viewer
-rsync -rav --delete $BASE/mobac/testsrc/build/libs/avnav-mapsources.zip $TARGET/downloads
-rsync -L -rav --delete --exclude=".idea" $BASE/raspberry/configui/ $TARGET/configGen
+rsync -rav --no-links --include=.htaccess --exclude-from="$BASE/exclude.sync" --delete $BASE/docs/ "$TARGET"/docs
+rsync -rav --no-links --include=.htaccess --exclude-from="$BASE/exclude.sync" $BASE/docs/downloads/ "$TARGET"/downloads
+rsync -rav --include="*php" --exclude="*" $BASE/docs/ "$TARGET"
+rsync -rav --delete $BASE/viewer/build/$SUB/ "$TARGET"/viewern
+rsync -rav --delete $BASE/viewer/demo/ "$TARGET"/viewer
+rsync -rav --delete $BASE/mobac/testsrc/build/libs/avnav-mapsources.zip "$TARGET"/downloads
+rsync -L -rav --delete --exclude=".idea" $BASE/raspberry/configui/ "$TARGET"/configGen
 
 chmod -R a+r $BASE
 
