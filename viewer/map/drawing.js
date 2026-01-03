0d9d6984d8661a0d5b5478f71ae711b6d38a09df
wellenvogel <andreas@wellenvogel.de>
2026-01-03 13:06:57 +0100

    correctly draw images and text on rotated views

--- viewer/map/drawing.js
+++ viewer/map/drawing.js
@@ -4,7 +4,7 @@
  * as all the ol3 stuff is hard to use or broken...
  */
 import {apply as olapply} from "ol/transform";
-
+import {add as oladd} from 'ol/coordinate';
 
 /**
  * an interface for converting between lat/lon and css pixel
@@ -136,12 +136,11 @@ Drawing.prototype.drawImageToContext=function(point,image,opt_options){
     if (! opt_options) opt_options={};
     if (image.naturalHeight == 0 || image.naturalWidth == 0) return; //silently ignore error
     let rt=this.pointToCssPixel(point);
-    let xy=this.pixelToDevice(rt);
     if (opt_options.fixX !== undefined) {
-        xy[0]=opt_options.fixX*this.devPixelRatio;
+        rt[0]=opt_options.fixX;
     }
     if (opt_options.fixY !== undefined) {
-        xy[1]=opt_options.fixY*this.devPixelRatio;
+        rt[1]=opt_options.fixY;
     }
     let devpixratio=this.devPixelRatio;
     let anchor=[0,0];
@@ -151,14 +150,17 @@ Drawing.prototype.drawImageToContext=function(point,image,opt_options){
     }
     let size;
     if (opt_options.size) {
-        size=opt_options.size;
+        size=opt_options.size.slice();
     }
     else {
         size=[image.naturalWidth,image.naturalHeight];
     }
     if (size[0]<=0 || size[1] <= 0) return;
+    size[0]=size[0]*devpixratio;
+    size[1]=size[1]*devpixratio;
     /** @type {CanvasRenderingContext2D} */
     let context=this.context;
+    let xy=this.pixelToDevice(rt);
     context.save();
     context.translate(xy[0],xy[1]);
     let angle=0;
@@ -171,17 +173,17 @@ Drawing.prototype.drawImageToContext=function(point,image,opt_options){
         context.beginPath();
         if (opt_options.background) {
             context.fillStyle = opt_options.background;
-            context.rect(-anchor[0], -anchor[1], size[0] * devpixratio, size[1] * devpixratio);
+            context.rect(-anchor[0], -anchor[1], size[0], size[1]);
         }
         else {
             context.fillStyle = opt_options.backgroundCircle;
-            context.arc(0, 0,Math.max(size[0] * devpixratio, size[1] * devpixratio)/2,0,2*Math.PI);
+            context.arc(0, 0,Math.max(size[0] , size[1] )/2,0,2*Math.PI);
         }
         this.setAlpha(opt_options,'backgroundAlpha');
         context.fill();
     }
     this.setAlpha(opt_options);
-    context.drawImage(image,-anchor[0],-anchor[1], size[0]*devpixratio, size[1]*devpixratio);
+    context.drawImage(image,-anchor[0],-anchor[1], size[0], size[1]);
     context.restore();
     return rt;
 };
@@ -364,7 +366,6 @@ Drawing.prototype.drawTextToContext=function(point,text,opt_styles){
     let doStroke=false;
     let noRotate=true;
     let rt=this.pointToCssPixel(point);
-    let dp=this.pixelToDevice(rt);
     let offset=[0,0];
     this.setAlpha(opt_styles);
     if (opt_styles) {
@@ -381,27 +382,29 @@ Drawing.prototype.drawTextToContext=function(point,text,opt_styles){
         if (opt_styles.width)this.context.lineWidth = this.useHdpi?opt_styles.width*this.devPixelRatio:opt_styles.width;
         if (opt_styles.rotateWithView) noRotate=false;
         if (opt_styles.fixX !== undefined) {
-            dp[0]=opt_styles.fixX*this.devPixelRatio;
+            rt[0]=opt_styles.fixX;
         }
         if (opt_styles.fixY !== undefined) {
-            dp[1]=opt_styles.fixY*this.devPixelRatio;
+            rt[1]=opt_styles.fixY
         }
-        if (opt_styles.offsetX) offset[0]=opt_styles.offsetX *this.devPixelRatio;
-        if (opt_styles.offsetY) offset[1]=opt_styles.offsetY *this.devPixelRatio;
+        if (opt_styles.offsetX) offset[0]=opt_styles.offsetX;
+        if (opt_styles.offsetY) offset[1]=opt_styles.offsetY;
     }
+    oladd(rt,offset);
+    const dp=this.pixelToDevice(rt);
     this.context.textAlign = opt_styles.align || 'center';
     this.context.textBaseline = opt_styles.baseline || 'middle';
     if (this.rotation && noRotate){
         this.context.save();
         this.context.translate(dp[0],dp[1]);
         this.context.rotate(-this.rotation);
-        if (doStroke) this.context.strokeText(text,offset[0],offset[1]);
-        if (doFill) this.context.fillText(text,offset[0],offset[1]);
+        if (doStroke) this.context.strokeText(text,0,0);
+        if (doFill) this.context.fillText(text,0,0);
         this.context.restore();
     }
     else {
-        if (doStroke) this.context.strokeText(text,dp[0]+offset[0],dp[1]+offset[1]);
-        if (doFill) this.context.fillText(text,dp[0]+offset[0],dp[1]+offset[1]);
+        if (doStroke) this.context.strokeText(text,dp[0],dp[1]);
+        if (doFill) this.context.fillText(text,dp[0],dp[1]);
     }
     return rt;
 };
