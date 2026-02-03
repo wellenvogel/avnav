115c0a041d0c7aed65ec1cdf92912c113935e767
wellenvogel <andreas@wellenvogel.de>
2026-02-03 20:04:37 +0100

    use apt -y

--- raspberry/xui/setup.sh
+++ raspberry/xui/setup.sh
@@ -38,13 +38,13 @@ fi
 if [ "$reinstalls" != "" ] ; then
     apt-get install --reinstall -y --allow-downgrades $reinstalls
 fi
-apt-get install python-xdg || apt-get install python3-xdg || err "unable to install"
+apt-get install -y python-xdg || apt-get install -y python3-xdg || err "unable to install"
 apt-get install -y --no-install-recommends xserver-xorg-video-all \
   xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils \
   onboard  at-spi2-core onboard-data mousetweaks gir1.2-ayatanaappindicator3-0.1 gir1.2-atspi-2.0 \
   openbox lxterminal dconf-cli firefox-esr dbus-x11 python3-xlib \
   nemo xfce4-panel mousepad xdotool menu libglib2.0-bin || err "unable to install"
-apt-get install gldriver-test || echo "gldriver-test not installed, X11 will not run on newer Pis"
+apt-get install -y gldriver-test || echo "gldriver-test not installed, X11 will not run on newer Pis"
 
 cp $pdir/avnav-startx.service /etc/systemd/system || err
 systemctl daemon-reload
