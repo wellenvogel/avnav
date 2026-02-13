6c5e573e93751dab62fe23a2c1981f39b0e02109
wellenvogel <andreas@wellenvogel.de>
2026-02-13 19:33:16 +0100

    intermediate: fetch interfaces and connections for nwm

--- raspberry/network-nm/plugin/plugin.mjs
+++ raspberry/network-nm/plugin/plugin.mjs
@@ -22,8 +22,94 @@
  */
 
  import htm from 'htm';
+ import {useState,useEffect,useCallback} from 'react';
+ const fetchStates={
+    0: undefined,
+    1: "reading devices",
+    2: "reading connections configurations",
+    4: "reading active connections",
+    8: "scanning"
+ };
+ const fetchData=async (url)=>{
+    const response=await fetch(url);
+    if (! response.ok) throw new Error(`error fetching ${url}: ${response.status}`);
+    const data=await response.json();
+    if (data.status != 'OK') throw new Error(`error fetching ${url}: ${data.status}`);
+    return data;
+ }
+
+ const nested=(obj,key)=>{
+    const parts=key.split('.')
+    for (let pp of parts){
+        if (typeof(obj) !== 'object') return;
+        if (! (pp in obj )) return;
+        obj=obj[pp];
+    }
+    return obj;
+ }
+
+ const Interface=({intf})=>{
+    return htm`<div className="interface dialogRow">${intf.interface}<//>`;
+ }
+ const Heading=htm`<h3>Wifi</h3>`
  const NetworkDialog=({api})=>{
-    return htm`<h1>Wifi Connections</h1>`
+    const [interfaces,setInterfaces]=useState([]);
+    const [activeConnections,setActiveConnections]=useState([]);
+    const [connections,setConnections]=useState([]);
+    const [networks,setNetworks]=useState([]);
+    const [fetchState,setFetchState]=useState(1);
+    const fetchItems=useCallback((url,setter,key)=>{
+        setFetchState((old)=>old | key);
+        fetchData(url)
+            .then ((response)=>{
+                setter(response.data);
+                setFetchState((old)=>old & (0xff ^ key));
+            })
+            .catch((error)=>{
+                api.showToast(error);
+                setFetchState((old)=>old & (0xff ^ key));
+            }
+            );
+
+    },[api,setFetchState]);
+    useEffect(()=>{
+        const intfUrl=api.getBaseUrl()+"api/devices?full=true&deviceType=Wi-Fi";
+        fetchItems(intfUrl,setInterfaces,1);
+        const acUrl=api.getBaseUrl()+"api/activeConnections?includeIpConfig=true&type=802-11-wireless";
+        fetchItems(acUrl,setActiveConnections,4);
+        const conUrl=api.getBaseUrl()+"api/connections?type=802-11-wireless";
+        fetchItems(conUrl,setConnections,2);
+        const nwUrl=api.getBaseUrl()+"api/scan";
+        fetchItems(nwUrl,setNetworks,8);
+    },[]);
+    if (fetchState != 0){
+        return htm`${Heading}
+        ${Object.keys(fetchStates).map((fs)=>(fetchState & fs)?htm`<div className="fetchState">${fetchStates[fs]}<//>`:null)}
+        `
+    }
+    //filter interfaces that we can use
+    //these need to have state "Activated" and either no active connection 
+    //or an connection of type infrastructure
+    const availableInterfaces=[];
+    (interfaces||[]).forEach((intf)=>{
+        if (intf.activeconnection){
+            //find the connection
+            for (let con of activeConnections){
+                if (con.path !== intf.activeconnection) continue;
+                if (nested(con,'connection.802-11-wireless.mode') !== 'infrastructure') return;
+                break;
+            }
+            availableInterfaces.push(intf);
+        }
+    });
+    if (availableInterfaces.length < 1){
+        return htm`${Heading}
+        <div className="dialogRow nointf">no free network interfaces</div>
+        `
+    }
+    return htm`<h3>Wifi</h3>
+    ${interfaces.map((intf)=>htm`<${Interface} intf=${intf}/>`)}
+    `
  }
 
  export default async (api)=>{
@@ -37,7 +123,8 @@
                 api.setStoreData(KVISIBLE,false);
                 dialogHandle=undefined;
             },
-            fullscreen: true
+            fullscreen: true,
+            className: 'nwmplugin WifiDialog'
         },ev);
         api.setStoreData(KVISIBLE,true);
         return rt;
