b7ed9afa94b0933077ba7a48cbc543b15435f3fb
wellenvogel <andreas@wellenvogel.de>
2026-07-07 19:24:47 +0200

    make osm-online availabel on windows for fresh install

--- windows/avnav.cmd
+++ windows/avnav.cmd
@@ -11,12 +11,25 @@ set "USE_PATH_FOR_GDAL_PYTHON=YES"
 setlocal enableextensions
 set "_AVNXML=%_AVNDATADIR%\avnav_server.xml"
 set "_AVNTMPL=%~dp0\avnav_server_win.xml"
+set "_AVNCHARTS=%_AVNDATADIR%\charts"
 if exist "%_AVNXML%" (
     rem
 ) else (
     if exist "%_AVNTMPL%" (
         copy /y "%_AVNTMPL%" "%_AVNXML%"
     )
+)
+if exist "%_AVNCHARTS%\" (
+    rem
+) else (
+    mkdir "%_AVNCHARTS%\"
+)
+set "_AVNDEMO=osm-online.xml"
+if exist "%_AVNCHARTS%\%_AVNDEMO" (
+    rem
+)
+else (
+    copy /y "%_AVNROOT%\viewer\demo\%_AVNDEMO%" "%_AVNCHARTS%\%_AVNDEMO%"
 )
 "%_AVNPYTHON%" "%_AVNMAIN%" %* -w "%_AVNDATADIR%" -u "viewer=%_AVNROOT%\viewer,sounds=%_AVNROOT%\sounds" "%_AVNXML%"
 endlocal
