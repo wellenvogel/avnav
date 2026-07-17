c95e586918f1aaf9cfd0ad344ae61c380256049e
wellenvogel <andreas@wellenvogel.de>
2026-07-17 17:47:02 +0200

    initial set up for new doc

--- newDoc/markdownify/convert.py
+++ newDoc/markdownify/convert.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
+import json
+import re
+
 from markdownify import markdownify as md, MarkdownConverter, UNDERLINED
 import sys
 import getopt
@@ -11,11 +14,14 @@ def warn(s):
 def error(s):
     print(f"ERROR: {s}")
 
+BTLISTPATH=re.compile('^\.\./images/icons-new/legacy/')
+OLDPATH=re.compile('.*viewerimages/icons-new/')
 class Converter(MarkdownConverter):
-    def __init__(self, imagePath=None,baseDir=None,**options):
+    def __init__(self, imagePath=None,baseDir=None,buttonMappings=None,**options):
         super().__init__(**options)
         self.imagePath = imagePath
         self.baseDir = baseDir
+        self.mappings=buttonMappings
 
     def convert_hN(self, n, el, text, parent_tags):
         anchor = None
@@ -44,6 +50,11 @@ class Converter(MarkdownConverter):
             if src.lower().startswith('http') or src.lower().startswith('/'):
                 pass
             else:
+                if self.mappings and OLDPATH.match(src):
+                    icon=OLDPATH.sub('',src)
+                    button=self.mappings.get(icon)
+                    if button:
+                        return '{{BT("'+button+'")}}'
                 src=os.path.join(imagepath,src)
                 if self.baseDir is not None:
                     isrc=os.path.join(self.baseDir,src)
@@ -53,14 +64,17 @@ class Converter(MarkdownConverter):
         rt=super().convert_img(el,text,parent_tags)
         return rt
 
-ARGS='i:'
-USAGE=f"usage: {sys.argv[0]} -i imageDir infile outfile"
+ARGS='i:b:'
+USAGE=f"usage: {sys.argv[0]} -i imageDir -b button.json infile outfile"
 if __name__ == "__main__":
     optlist,args=getopt.getopt(sys.argv[1:],ARGS)
     imagepath=None
+    buttonjson=None
     for o,a in optlist:
         if o=='-i':
             imagepath=a
+        elif o == '-b':
+            buttonjson=a
         else:
             assert False, "unhandled option"
     if len(args)  < 2:
@@ -72,10 +86,20 @@ if __name__ == "__main__":
         print(f"infile {infile} not found",file=sys.stderr)
         sys.exit(1)
     outdir = os.path.dirname(outfile)
+    buttons={}
     if imagepath is not None:
         #compute relpath
         imagepath=os.path.relpath(imagepath,outdir)
         log(f"imagepath: {imagepath}")
+    if buttonjson is not None:
+        log(f"reading buttonjson: {buttonjson}")
+        with open(buttonjson,"r") as f:
+            cfg=json.load(f)
+            for k,v in cfg.items():
+                icon=v.get('legacy') #only need to compare legacy icons
+                if icon is not None:
+                    icon=BTLISTPATH.sub("",icon)
+                    buttons[icon]=k
     with open(infile, "r") as f:
         out=Converter(imagePath=imagepath,baseDir=outdir).convert(f.read())
         with open(outfile, "w") as fo:
