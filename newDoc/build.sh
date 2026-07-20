27b810de5e38da3bf9dd04643d16acd03084e906
wellenvogel <andreas@wellenvogel.de>
2026-07-20 17:26:19 +0200

    update the build script to run the button usage creation already in docker

--- newDoc/build.sh
+++ newDoc/build.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-IMAGE="wellenvogel/avnav-doc-build:1.2"
+IMAGE="wellenvogel/avnav-doc-build:1.3"
 usage(){
     echo "usage: $0 [-d] [-h] [-p port] [<command>]"
 }
 useDocker=0
 port=8000
-while getopts "dhp" arg; do
+address="127.0.0.1"
+pdir=`dirname $0`
+pdir=`readlink -f $pdir`
+while getopts "dhp:a:" arg; do
   case "$arg" in
     d)
       useDocker=1
@@ -15,6 +18,9 @@ while getopts "dhp" arg; do
       ;;
     p)
       port=$OPTARG
+      ;;
+    a)
+      address=$OPTARG
       ;;
   esac
 done
@@ -29,10 +35,12 @@ err(){
     echo "ERROR: $*"
     exit 1
 }
+if [ $useDocker = 1 ] ; then
+  docker run -ti --rm -u`id -u` -v "`readlink -f $pdir/..`:/app" -p8000:$port "$IMAGE" /app/newDoc/build.sh -a 0.0.0.0 $command
+  exit $?
+fi
 
 #build the button usage files
-pdir=`dirname $0`
-pdir=`readlink -f $pdir`
 script="$pdir/../tools/buttonUsage.py"
 [ ! -x "$script" ]  && err "$script not found/not executable"
 gendir="$pdir/docs/generated"
@@ -47,16 +55,10 @@ outfile="$pdir/docs/generated/buttons.json"
 $script -f buttonjson -i "$icondir" -o "$outfile" || err "unable to create $outfile"
 
 cd $pdir || err ""
-if [ $useDocker = 1 ] ; then
-  if [ "$command" = "serve" ] ; then
-    command="$command --dev-addr 0.0.0.0:8000 "
-  fi
-  docker run -ti --rm -u`id -u` -v "`readlink -f $pdir/..`:/app" -p8000:$port "$IMAGE" mkdocs `echo $command`
+if [ "$command" = "serve" ] ; then
+  mkdocs $command --dev-addr $address:$port
 else
-  if [ "$command" = "serve" ] ; then
-    command="$command --dev-addr 127.0.0.1:$port"
-  fi
-  mkdocs `echo $command`
+  mkdocs $command
 fi
 
 
